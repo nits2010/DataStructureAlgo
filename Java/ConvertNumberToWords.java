6050976b6dfe43a6b6ae942dd8a126562ec341f6
ngupt10 <nitin.gupta@walmart.com>
2019-03-15 01:04:17 +0530

    floating point number to words

--- Java/ConvertNumberToWords.java
+++ Java/ConvertNumberToWords.java
@@ -48,11 +48,10 @@ public class ConvertNumberToWords {
     private static String convertLessThanOneThousand(int number) {
         String soFar;
 
-        if (number % 100 < 20){
+        if (number % 100 < 20) {
             soFar = numNames[number % 100];
             number /= 100;
-        }
-        else {
+        } else {
             soFar = numNames[number % 10];
             number /= 10;
 
@@ -64,9 +63,31 @@ public class ConvertNumberToWords {
     }
 
 
-    public static String convert(long number) {
+    private static String convertLessThanOneThousandWithAnd(int number) {
+        String soFar;
+
+        if (number % 100 < 20) {
+            soFar = numNames[number % 100];
+            number /= 100;
+        } else {
+            soFar = numNames[number % 10];
+            number /= 10;
+
+            soFar = tensNames[number % 10] + soFar;
+            number /= 10;
+        }
+        if (number == 0) return soFar;
+        if (!soFar.isEmpty())
+            return numNames[number] + " hundred and " + soFar;
+        return numNames[number] + " hundred" + soFar;
+    }
+
+
+    public static String convert(long number, boolean withoutAnd) {
         // 0 to 999 999 999 999
-        if (number == 0) { return "zero"; }
+        if (number == 0) {
+            return "zero";
+        }
 
         String snumber = Long.toString(number);
 
@@ -76,90 +97,114 @@ public class ConvertNumberToWords {
         snumber = df.format(number);
 
         // XXXnnnnnnnnn
-        int billions = Integer.parseInt(snumber.substring(0,3));
+        int billions = Integer.parseInt(snumber.substring(0, 3));
         // nnnXXXnnnnnn
-        int millions  = Integer.parseInt(snumber.substring(3,6));
+        int millions = Integer.parseInt(snumber.substring(3, 6));
         // nnnnnnXXXnnn
-        int hundredThousands = Integer.parseInt(snumber.substring(6,9));
+        int hundredThousands = Integer.parseInt(snumber.substring(6, 9));
         // nnnnnnnnnXXX
-        int thousands = Integer.parseInt(snumber.substring(9,12));
+        int thousands = Integer.parseInt(snumber.substring(9, 12));
 
         String tradBillions;
         switch (billions) {
             case 0:
                 tradBillions = "";
                 break;
-            case 1 :
+            case 1:
                 tradBillions = convertLessThanOneThousand(billions)
                         + " billion ";
                 break;
-            default :
+            default:
                 tradBillions = convertLessThanOneThousand(billions)
                         + " billion ";
         }
-        String result =  tradBillions;
+        String result = tradBillions;
 
         String tradMillions;
         switch (millions) {
             case 0:
                 tradMillions = "";
                 break;
-            case 1 :
+            case 1:
                 tradMillions = convertLessThanOneThousand(millions)
                         + " million ";
                 break;
-            default :
+            default:
                 tradMillions = convertLessThanOneThousand(millions)
                         + " million ";
         }
-        result =  result + tradMillions;
+        result = result + tradMillions;
 
         String tradHundredThousands;
         switch (hundredThousands) {
             case 0:
                 tradHundredThousands = "";
                 break;
-            case 1 :
+            case 1:
                 tradHundredThousands = "one thousand ";
                 break;
-            default :
+            default:
                 tradHundredThousands = convertLessThanOneThousand(hundredThousands)
                         + " thousand ";
         }
-        result =  result + tradHundredThousands;
+        result = result + tradHundredThousands;
 
         String tradThousand;
-        tradThousand = convertLessThanOneThousand(thousands);
-        result =  result + tradThousand;
+        if (!withoutAnd)
+            tradThousand = convertLessThanOneThousandWithAnd(thousands);
+        else
+            tradThousand = convertLessThanOneThousand(thousands);
+        result = result + tradThousand;
 
         // remove extra spaces!
         return result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ");
     }
 
+
+    static String convert(float number) {
+
+        String temp = String.valueOf(number);
+
+        String v[] = temp.split("\\.");
+
+        String left = convert(Long.parseLong(v[0]), true);
+
+        String right = convert(Long.parseLong(v[1]), true);
+
+        return left + " and " + right;
+
+
+    }
+
     /**
      * testing
+     *
      * @param args
      */
     public static void main(String[] args) {
-//        System.out.println("*** " + convert(0));
-//        System.out.println("*** " + convert(1));
-//        System.out.println("*** " + convert(16));
-//        System.out.println("*** " + convert(100));
-//        System.out.println("*** " + convert(118));
-//        System.out.println("*** " + convert(200));
-//        System.out.println("*** " + convert(219));
-//        System.out.println("*** " + convert(800));
-//        System.out.println("*** " + convert(801));
-//        System.out.println("*** " + convert(1316));
-//        System.out.println("*** " + convert(1000000));
-//        System.out.println("*** " + convert(2000000));
-//        System.out.println("*** " + convert(3000200));
-//        System.out.println("*** " + convert(700000));
-//        System.out.println("*** " + convert(9000000));
-        System.out.println("*** " + convert(9001000));
-//        System.out.println("*** " + convert(123456789));
-//        System.out.println("*** " + convert(2147483647));
-//        System.out.println("*** " + convert(3000000010L));
+
+        System.out.println("*** " + convert(334.32f ));
+
+
+//        System.out.println("*** " + convert(0, true));
+//        System.out.println("*** " + convert(1, true));
+//        System.out.println("*** " + convert(16, true));
+//        System.out.println("*** " + convert(100, true));
+//        System.out.println("*** " + convert(118, true));
+//        System.out.println("*** " + convert(200, true));
+//        System.out.println("*** " + convert(219, true));
+//        System.out.println("*** " + convert(800, true));
+//        System.out.println("*** " + convert(801, true));
+//        System.out.println("*** " + convert(1316, true));
+//        System.out.println("*** " + convert(1000000, true));
+//        System.out.println("*** " + convert(2000000, true));
+//        System.out.println("*** " + convert(3000200, true));
+//        System.out.println("*** " + convert(700000, true));
+//        System.out.println("*** " + convert(9000000, true));
+//        System.out.println("*** " + convert(9001000, true));
+//        System.out.println("*** " + convert(123456789, true));
+//        System.out.println("*** " + convert(2147483647, true));
+//        System.out.println("*** " + convert(3000000010L, true));
 
         /*
          *** zero
