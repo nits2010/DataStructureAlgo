ded9737cf892e69808a225128c9ce3e79cf1ea9d
ngupt10 <nitin.gupta@walmart.com>
2019-04-04 18:05:19 +0530

    more facebook interview questions

---
+++ Java/StringOddEvenTransformationInPlaceOrderKeep.java
@@ -0,0 +1,120 @@
+package Java;
+
+import java.util.Arrays;
+
+/**
+ * Author: Nitin Gupta(nitin.gupta@walmart.com)
+ * Date: 04/04/19
+ * Description:
+ */
+public class StringOddEvenTransformationInPlaceOrderKeep {
+
+
+    public static void main(String arg[]) {
+        String input = "a1b2c3d4e5f6g7h8i9";
+
+        String[] in = {"a", "1", "b", "2", "c", "3", "d", "4", "e", "5", "f", "6", "g", "7", "h", "8", "i", "9", "j", "10", "k", "11"};
+
+
+        Arrays.stream(transform(in)).forEach(x -> System.out.print(x + " "));
+    }
+
+    private static String[] transform(String[] input) {
+
+        String str[] = input;
+        int shift = 0;
+        int length = input.length;
+        int partitionLength;
+        int currentStringLength;
+
+        int leftOverLength = length;
+
+        //Iterate over string, till every partition transformed
+        while (leftOverLength > 0) {
+            partitionLength = getPartitionLength(leftOverLength);
+            currentStringLength = partitionLength(partitionLength);
+            leftOverLength = leftOverLength - currentStringLength;
+
+
+            //Apply cycle leader algo
+            cycleLeaderAlgorithm(str, shift, currentStringLength);
+
+            //Reverse the second half of first substring
+            reverse(str, shift / 2, shift - 1);
+
+            //reverse the first half of second substring
+            reverse(str, shift, shift + currentStringLength / 2 - 1);
+
+            //reverse the second half of first substring and first half of second substring toegether
+            reverse(str, shift / 2, shift + currentStringLength / 2 - 1);
+
+            shift += currentStringLength;
+
+
+        }
+        return str;
+
+
+    }
+
+    private static void swap(String input[], int firstIndex, int secondIndex) {
+        String temp = input[firstIndex];
+        input[firstIndex] = input[secondIndex];
+        input[secondIndex] = temp;
+    }
+
+    private static void reverse(String in[], int from, int to) {
+
+        while (from < to) {
+            swap(in, from, to);
+            from++;
+            to--;
+        }
+
+    }
+
+    private static void cycleLeaderAlgorithm(String[] str, int shift, int partitionLength) {
+
+
+        String temp;
+
+        for (int i = 1; i < partitionLength; i = i * 3) {
+
+            String current = str[shift + i];
+            int j = i;
+
+            do {
+
+                //Even index
+                if (j % 2 == 0)
+                    j = j / 2;
+
+                else
+                    j = partitionLength / 2 + j / 2;
+
+                temp = str[shift + j];
+
+                str[shift + j] = current;
+
+                current = temp;
+
+            } while (j != i);
+
+        }
+
+    }
+
+    static int partitionLength(int partitionLength) {
+        return (int) Math.pow(3, partitionLength) + 1;
+    }
+
+    static int getPartitionLength(int length) {
+
+        int k = 0;
+
+        while (Math.pow(3, k) + 1 <= length)
+            k++;
+
+        return k - 1;
+    }
+}
---
+++ facebook/MinimumBreak.java
@@ -0,0 +1,152 @@
+package facebook;
+
+import Java.Design.parkingLot.parkingLot.ParkingLot;
+
+import java.util.*;
+
+/**
+ * Author: Nitin Gupta(nitin.gupta@walmart.com)
+ * Date: 04/04/19
+ * Description: https://www.geeksforgeeks.org/minimum-word-break/
+ * <p>
+ * Given a dictionary ["Cat", "Mat", "Ca",
+ * "tM", "at", "C", "Dog", "og", "Do"]
+ * <p>
+ * Input :  Pattern "CatMat"
+ * Output : 1
+ * Explanation: we can break the sentences
+ * in three ways, as follows:
+ * CatMat = [ Cat Mat ]  break 1
+ * CatMat = [ Ca tM at ] break 2
+ * CatMat = [ C at Mat ] break 2  so the
+ * output is: 1
+ * <p>
+ * <p>
+ * Approach 1:
+ * a. We can assume the dictionary as hash map.
+ * b. starting from 0 cut require to n cut require for (int cut =0; cut<n; cut++) where n is the size of pattern
+ * c. for each cut we see the substring is in map or not, if its then this number of cut is required
+ * <p>
+ * Approach 2:
+ * 1. Build the trie of given dic
+ * 2. Start searching the pattern till it either hit the end of pattern or a leaf node in trie
+ * 2.1 if it hit a leaf node in trie, and pattern still remaining that means we have to cut the pattern here at least, increment the count and
+ * recursively apply this on rest of the pattern
+ * 2.2 if it hit the leaf node in trie and pattern is exhaust then return the min cut so far.
+ */
+public class MinimumBreak {
+
+
+    static class TrieNode {
+
+        char value;
+        Map<Character, TrieNode> childrens = new HashMap<>();
+        boolean isLeaf = false;
+
+        public TrieNode() {
+        }
+
+        public TrieNode(char value) {
+            this.value = value;
+        }
+    }
+
+    static class Trie {
+
+        TrieNode root;
+
+        public void insert(String toInsert) {
+
+            if (root == null)
+                root = new TrieNode();
+
+            TrieNode pCrawl = root;
+
+            for (int i = 0; i < toInsert.length(); i++) {
+                char current = toInsert.charAt(i);
+
+                if (!pCrawl.childrens.containsKey(current))
+                    pCrawl.childrens.put(current, new TrieNode(current));
+
+                pCrawl = pCrawl.childrens.get(current);
+            }
+
+            pCrawl.isLeaf = true;
+        }
+
+        public int search(String toSearch) {
+            TrieNode pCrawl = root;
+            int lastMatchPoint = -1;
+            int i;
+            for (i = 0; i < toSearch.length(); i++) {
+                char current = toSearch.charAt(i);
+
+
+                if (pCrawl.childrens.containsKey(current))
+                    pCrawl = pCrawl.childrens.get(current);
+                else
+                    break;
+
+
+                if (pCrawl.isLeaf)
+                    lastMatchPoint = i;
+
+
+            }
+
+            if (pCrawl.isLeaf)
+                return (lastMatchPoint == -1) ? i : lastMatchPoint;
+
+            return lastMatchPoint;
+
+
+        }
+    }
+
+
+    public static int minimumCut(String pattern, Trie trie) {
+
+        String temp = pattern;
+        int remaining = temp.length();
+        int minCut = 0;
+        int lastMatchPoint = 0;
+
+        while (remaining > 0) {
+
+            lastMatchPoint = trie.search(temp);
+
+            if (lastMatchPoint == -1) { // If this is not found, then its impossible to break it because this string / character is not present in dic;
+                /**
+                 * Example:  dictionary[] = {"Cat", "Mat", "Ca", "tM", "at", "C", "Dog", "og", "Do"};
+                 * Pattern= Dogcat ; Dog is found but cat or 'c' it self not found hence not possible
+                 */
+
+                return -1;
+            } else if (lastMatchPoint + 1 < temp.length()) { //if Found, then increase the cut if require otherwise we are done with cut
+                minCut++;
+                temp = temp.substring(lastMatchPoint + 1);
+                remaining -= lastMatchPoint + 1;
+            } else return minCut;
+
+        }
+
+
+        return minCut;
+
+    }
+
+    public static void main(String args[]) {
+        String dictionary[] = {"Cat", "Mat", "Ca", "tM", "at", "C", "Dog", "og", "Do"};
+
+        Trie trie = new Trie();
+        Arrays.stream(dictionary).forEach(x -> trie.insert(x));
+        System.out.println(minimumCut("Dogcat", trie));
+        System.out.println(minimumCut("DogCat", trie));
+        System.out.println(minimumCut("CtMMat", trie));
+        System.out.println(minimumCut("CatMat", trie));
+
+
+    }
+
+
+}
