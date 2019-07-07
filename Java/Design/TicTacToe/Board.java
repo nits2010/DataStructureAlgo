8b786a95304082c655f862d367c4cfbd9ee84666
ngupt10 <nitin.gupta@walmart.com>
2019-07-07 14:38:38 +0530

    tictactoe

---
+++ Java/Design/TicTacToe/Board.java
@@ -0,0 +1,22 @@
+package Java.Design.TicTacToe;
+
+/**
+ * Author: Nitin Gupta(nitin.gupta@walmart.com)
+ * Date: 2019-07-07
+ * Description:
+ */
+public class Board {
+
+    private int board[][];
+    private int player1, player2;
+    private int rowSum[];
+    private int colSum[];
+    private int digSum[];
+    private int revDigSum[];
+    private int boardLenth;
+
+    public Board(int boardLenth) {
+
+    }
+
+}
