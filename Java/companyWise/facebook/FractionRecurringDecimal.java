3d7b65fa343cf798ed23c4e056db91c87f69bf0a
Nitin Gupta <iit.gupta2010@gmail.com>
2019-07-20 07:34:16 -0700

    Google/PoemRythms.java, facebook/SimilarRGBColor

--- Java/companyWise/facebook/FractionRecurringDecimal.java
+++ Java/companyWise/facebook/FractionRecurringDecimal.java
@@ -1,4 +1,4 @@
-package Java;
+package Java.companyWise.facebook;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,7 +6,25 @@ import java.util.Map;
 /**
  * Author: Nitin Gupta(nitin.gupta@walmart.com)
  * Date: 2019-06-30
- * Description:
+ * Description: https://leetcode.com/problems/fraction-to-recurring-decimal/
+ * Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+ * <p>
+ * If the fractional part is repeating, enclose the repeating part in parentheses.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: numerator = 1, denominator = 2
+ * Output: "0.5"
+ * Example 2:
+ * <p>
+ * Input: numerator = 2, denominator = 1
+ * Output: "2"
+ * Example 3:
+ * <p>
+ * Input: numerator = 2, denominator = 3
+ * Output: "0.(6)"
+ * <p>
+ * [FACEBOOK]
  */
 public class FractionRecurringDecimal {
 
@@ -36,7 +54,7 @@ public class FractionRecurringDecimal {
 
         // quotient
         long res = num / den;
-        result += String.valueOf(res);
+        result += res;
 
         // if remainder is 0, return result
         long remainder = num % den;
@@ -46,21 +64,24 @@ public class FractionRecurringDecimal {
         // right-hand side of decimal point
         Map<Long, Integer> map = new HashMap<>();
         result += ".";
-        remainder = remainder * 10;
+
+        remainder = remainder * 10; //start multiplying and divide and collect
+
         while (remainder != 0) {
+
             // if digits repeat
             if (map.containsKey(remainder)) {
                 int beg = map.get(remainder);
-                String part1 = result.substring(0, beg);
-                String part2 = result.substring(beg);
-                result = part1 + "(" + part2 + ")";
+                String part1 = result.substring(0, beg);  //get the part before fraction
+                String part2 = result.substring(beg); //get the part after fraction
+                result = part1 + "(" + part2 + ")"; //attach them
                 return result;
             }
 
             // continue
-            map.put(remainder, result.length());
-            res = remainder / den;
-            result += String.valueOf(res);
+            map.put(remainder, result.length()); //putting length of current string will make sure that we get right length of fractional numbers which are repeated
+            res = remainder / den; //the the quotient
+            result += String.valueOf(res); //attach this quotient which probably a repeated number in ()
             remainder = (remainder % den) * 10;
         }
 
