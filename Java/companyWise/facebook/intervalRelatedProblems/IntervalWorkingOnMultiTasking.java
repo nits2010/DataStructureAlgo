4904dfb4da7b5c8324e953ce5cb0fc6e39f407ce
Nitin Gupta <iit.gupta2010@gmail.com>
2019-07-21 19:42:40 +0530

    NumberSubarraysBoundedMaximum, /IntervalWorkingOnMultiTasking

--- Java/companyWise/facebook/intervalRelatedProblems/IntervalWorkingOnMultiTasking.java
+++ Java/companyWise/facebook/intervalRelatedProblems/IntervalWorkingOnMultiTasking.java
@@ -1,5 +1,7 @@
 package Java.companyWise.facebook.intervalRelatedProblems;
 
+import javafx.util.Pair;
+
 import java.util.*;
 
 /**
@@ -18,18 +20,15 @@ import java.util.*;
  * Output:
  * union [[1, 12], [14, 17]]
  * intersection: [[2, 6], [9, 10]]
+ * <p>
+ * https://leetcode.com/discuss/interview-question/338948/facebook-onsite-schedule-of-tasks/309123
+ * [FACEBOOK]
  */
 
 
-class Interval implements Cloneable {
+class Interval {
     int start, end;
-    boolean isMerged = false;
 
-
-    @Override
-    protected Interval clone() {
-        return new Interval(this.start, this.end);
-    }
 
     public Interval(int start, int end) {
         this.start = start;
@@ -118,203 +117,12 @@ public class IntervalWorkingOnMultiTasking {
 
 
     private static void test(List<Interval> intervals) {
-        System.out.print("\n\n\n Testing intervals  " + intervals);
+        System.out.print("\n\nIntervals  " + intervals);
 
-        WorkingMultiTasking solll = new WorkingMultiTasking();
-        solll.workingMultiTasking(intervals);
+        WorkingMultiTasking sol = new WorkingMultiTasking();
+        System.out.println("\nUsing (n*Logn) algorithm :" + sol.workingMultiTasking(intervals));
+        System.out.println("Using (n) algorithm :" + sol.workingMultiTaskingLinear(intervals));
 
-//
-//        SolutionIntervalWorkingOnMultiTasking sol = new SolutionIntervalWorkingOnMultiTasking();
-//
-//        System.out.println("intervals you are working : " + sol.workingOn(intervals));
-//        System.out.println("what intervals you are multi-tasking : " + sol.multiTasking(intervals));
-
-
-    }
-
-
-}
-
-class SolutionIntervalWorkingOnMultiTasking {
-
-
-    /**
-     * Working on intervals would be those intervals which are overlapping and get merged (union).
-     *
-     * @param intervals
-     * @return
-     */
-    public List<Interval> workingOn(List<Interval> intervals) {
-
-        if (intervals == null || intervals.isEmpty())
-            return intervals;
-
-        /**
-         * will hold those intervals we are working on.
-         */
-        final List<Interval> workingOnIntervals = new ArrayList<>(intervals.size());
-
-        /**
-         * Since the intervals are already sorted by start time, we can skip sorting. In case needed then uncomment the below code
-         */
-
-        // Sort them by start time / if clash then end time
-//        Collections.sort(intervals, (o1, o2) -> {
-//            if (o1.start == o2.start)
-//                return o1.end - o2.end; //if clash then end time
-//            return o1.start - o2.start; //sort them by start time
-//        });
-
-
-        //Pick the first interval as our solution, as this will always be there in final list [ since data is sorted by start time ]
-        workingOnIntervals.add(new Interval(intervals.get(0).start, intervals.get(0).end));
-
-
-        //Iterate through the list and merge the overlapping intervals
-        for (int i = 1; i < intervals.size(); i++) {
-
-            Interval previousInterval = workingOnIntervals.get(workingOnIntervals.size() - 1);
-            Interval currentInterval = intervals.get(i);
-
-            if (areOverlap(previousInterval, currentInterval)) {
-
-                //merge this to interval: take Union;
-                previousInterval.start = Math.min(previousInterval.start, currentInterval.start);
-                previousInterval.end = Math.max(previousInterval.end, currentInterval.end);
-
-                //don't need to add back to list, as this is the reference of last interval, it will automatically get reflected due to referencing.
-            } else {
-                //If they don't overlap, then we need to start a new interval;
-                workingOnIntervals.add(new Interval(currentInterval.start, currentInterval.end));
-            }
-
-        }
-
-        return workingOnIntervals;
-    }
-
-    /**
-     * Check given intervals are overlapping intervals;
-     * They overlap if
-     * a.end >= b.start && b.end >= a.start
-     *
-     * @param a
-     * @param b
-     * @return
-     */
-    private boolean areOverlap(Interval a, Interval b) {
-
-        return (a.end >= b.start && b.end >= a.start);
-
-    }
-
-    /**
-     * Multi tasking interval would be those interval where people are multi tasking between intervals.
-     * For example:
-     * [1,10][2,6][9,12]
-     * <p>
-     * if we draw the line of intervals, we'll see during working on [1,10], we took another job at interval [2,6] {this is within this interval} and [9,12] {which is partially within this interval}
-     * This drives two types of intervals;
-     * 1. Those intervals which are completely within this interval like [1,10] & [2,6] ; we'll chose [2,6] but keep [1,10] till we find all the within and partially overlapping intervals.
-     * 2. Those intervals which are partially overlapping with this interval like [1,10] and [9,12]; here these two interval partially overlap the we'll do intersection and discard [1,10] as this
-     * interval we have find out all the overlapping and partially overlapping interval.
-     * <p>
-     * <p>
-     * How to find the first and second intervals;  [Hint: keep in mind they are sorted by start time]
-     * Say two interval 'a' and 'b';
-     * 1.  if a.start <=b.start && a.end>=b.end; this means b is within the interval of 'a'
-     * 2.  if a.end >= b.start and a.end<=b.end  this means they are partially overlap because a.end>=b.start which says 'b' started before 'a' ends and a.end<=b.end says 'a' gets end before
-     * 'b' got end.
-     *
-     * @param intervals
-     * @return
-     */
-
-    public List<Interval> multiTasking(List<Interval> intervals) {
-
-        if (intervals == null || intervals.isEmpty())
-            return intervals;
-
-
-        List<Interval> multiTasking = new ArrayList<>(intervals.size());
-
-        Stack<Interval> stack = new Stack<>();
-
-        stack.push(intervals.get(0));
-
-        for (int i = 1; i < intervals.size(); i++) {
-
-            if (!stack.isEmpty() && isWithinInterval(stack.peek(), intervals.get(i))) {
-                multiTasking.add(intervals.get(i));
-                continue;
-            }
-
-            if (!stack.isEmpty() && isPartiallyOverlap(stack.peek(), intervals.get(i))) {
-                stack.peek().start = Math.max(stack.peek().start, intervals.get(i).start);
-                stack.peek().end = Math.min(stack.peek().end, intervals.get(i).end);
-
-                stack.peek().isMerged = true;
-                if (stack.peek().start == stack.peek().end)
-                    stack.pop();
-
-                continue;
-            }
-
-            if (!stack.isEmpty() && isOutSide(stack.peek(), intervals.get(i))) {
-                Interval pop = stack.pop();
-                if (pop.isMerged)
-                    multiTasking.add(pop);
-
-                stack.push(intervals.get(i));
-
-
-            }
-        }
-
-
-        return multiTasking;
-    }
-
-
-    /**
-     * Those intervals which are completely within this interval like [1,10] & [2,6]
-     * <p>
-     * 1. (a.start <= b.start && a.end >= b.end);
-     * Or
-     * 2. a.end >= b.start && a.end >=b.end since intervals are sorted and we know that start < end
-     *
-     * @param a
-     * @param b
-     * @return
-     */
-    private boolean isWithinInterval(Interval a, Interval b) {
-
-        return (a.start <= b.start && a.end >= b.end);
-    }
-
-    /**
-     * Those intervals which are partially overlapping with this interval like [1,10] and [9,12];
-     *
-     * @param a
-     * @param b
-     * @return
-     */
-    private boolean isPartiallyOverlap(Interval a, Interval b) {
-
-//        return (a.end >= b.start && a.end <= b.end); if (temp[k][1] >= intervals[i][0] && intervals[i][1] >= temp[k][0]) {
-        return (a.end >= b.start && a.start <= b.end);
-    }
-
-    /**
-     * are these interval out side to each other ;
-     * like [2,6] and [7,12];
-     *
-     * @param a
-     * @param b
-     * @return
-     */
-    private boolean isOutSide(Interval a, Interval b) {
-        return (a.end < b.start);
     }
 
 
@@ -323,21 +131,8 @@ class SolutionIntervalWorkingOnMultiTasking {
 class WorkingMultiTasking {
 
 
-    class Solution {
-
-        List<Interval> working = new ArrayList<>();
-        List<Interval> multiTasking = new ArrayList<>();
-
-        @Override
-        public String toString() {
-            return "{" +
-                    "working=" + working +
-                    ", multiTasking=" + multiTasking +
-                    '}';
-        }
-    }
-
     /**
+     * O(nlgon)
      * Often the trick for analyzing overlapping intervals is to make a sorted list (or tree-map) of the individual events at either end of the intervals. Note that if the range of possible time-points is very small (like 0..23, for example), it would be more efficient to use a static array for this.
      * <p>
      * We iterate over the list of events, and check how each event changes the concurrency-counter:
@@ -348,10 +143,15 @@ class WorkingMultiTasking {
      * Down to 1 or 0: close the last multitasking interval.
      *
      * @param intervals
+     * @return
      */
-    void workingMultiTasking(List<Interval> intervals) {
+    Pair<List<Interval>, List<Interval>> workingMultiTasking(List<Interval> intervals) {
+
 
         Map<Integer, Integer> map = new TreeMap<>();
+
+        List<Interval> working = new ArrayList<>();
+        List<Interval> multiTasking = new ArrayList<>();
 
         for (Interval i : intervals) {
 
@@ -359,7 +159,6 @@ class WorkingMultiTasking {
             map.put(i.end, map.getOrDefault(i.end, 0) - 1);
         }
 
-        Solution solution = new Solution();
         int next = 0;
         for (Integer event : map.keySet()) {
 
@@ -367,16 +166,103 @@ class WorkingMultiTasking {
             int prev = next;
             next += map.get(event);
 
-            if (prev <= 0 && 1 <= next) solution.working.add(new Interval(time, 0));
-            if (prev <= 1 && 2 <= next) solution.multiTasking.add(new Interval(time, 0));
-            if (prev >= 1 && 0 >= next) solution.working.get(solution.working.size() - 1).end = time;
-            if (prev >= 2 && 1 >= next) solution.multiTasking.get(solution.multiTasking.size() - 1).end = time;
+            if (prev <= 0 && 1 <= next) working.add(new Interval(time, 0));
+            if (prev <= 1 && 2 <= next) multiTasking.add(new Interval(time, 0));
+            if (prev >= 1 && 0 >= next) working.get(working.size() - 1).end = time;
+            if (prev >= 2 && 1 >= next) multiTasking.get(multiTasking.size() - 1).end = time;
         }
+        return new Pair<>(working, multiTasking);
 
-        System.out.println(solution);
 
     }
 
+
+    /**
+     * O(n)
+     * <p>
+     * The problem we're trying to solve is that although the start-times are sorted, we can't assume that the end-times are sorted too.
+     * Fortunately, we don't need to know all the end-times in order, because we don't need to know when concurrency drops from 4 tasks to 3 tasks, or goes up from 5 to 6.
+     * All we care about is when concurrency changes between 0, 1 and 2 tasks.
+     * That means that as we iterate over the tasks, we only need to keep track of the last end-time we've encountered, and (if we're currently nested) the second-to-last.
+     *
+     * @param intervals
+     * @return
+     */
+    Pair<LinkedList<Interval>, LinkedList<Interval>> workingMultiTaskingLinear(final List<Interval> intervals) {
+
+
+        /**
+         * This will hold a interval i was working
+         */
+        LinkedList<Interval> working = new LinkedList<>();
+
+        /**
+         * This will hold at what interval i was multitasking
+         */
+        LinkedList<Interval> multiTasking = new LinkedList<>();
+
+        /**
+         * This two will just be holding the end time of working and multitasking.
+         * AS i've no task right now so its -1
+         */
+        int lastWorkingEndTime = -1;
+        int lastMultiTaskingEndTime = -1;
+
+
+        for (final Interval interval : intervals) {
+
+
+            /**
+             * 1. Do we have any task right now?  working.isEmpty() : No then take a task and update my endTime
+             * 2. Am i doing multitask? Is this new task come before I finished the last task? lastWorkingEndTime < interval.start ; if yes it means i'm multitasking
+             */
+            if (working.isEmpty() || lastWorkingEndTime < interval.start) {
+                working.add(new Interval(interval.start, interval.end));
+                lastWorkingEndTime = working.getLast().end;
+
+            } else {
+
+                /**
+                 * Since this new task come before i finished the last task?, then i'm multi-tasking for sure
+                 */
+                if (lastWorkingEndTime > interval.start) {
+
+                    /**
+                     * 1. Do i have any interval i was multi tasking ? multiTasking.isEmpty(); if not then add the current task as my multi tasking becuase we identify that i'm doing multitask
+                     * 2. Is the new task start after my last multi task time ? lastMultiTaskingEndTime < interval.start means i've must started another multi task at different interval
+                     */
+                    if (multiTasking.isEmpty() || lastMultiTaskingEndTime < interval.start) {
+                        //Intersection of intervals
+
+                        int start = interval.start; //Note when i stared the new multi task work
+                        int end = Math.min(interval.end, lastWorkingEndTime); //since i'm doing multi tasking, then the end time should be within my last working task (then only i'm doing multi task)
+
+                        multiTasking.add(new Interval(start, end));
+
+                        lastMultiTaskingEndTime = end;
+
+
+                    } else if (lastMultiTaskingEndTime < interval.end) {
+
+                        //Intersection of intervals
+                        int end = Math.min(interval.end, lastWorkingEndTime);
+
+
+                        multiTasking.getLast().end = end;
+                        lastMultiTaskingEndTime = end;
+                    }
+
+                }
+
+                //Find till what time i'm doing the task; Union of intervals
+                working.getLast().end = Math.max(lastWorkingEndTime, interval.end);
+                lastWorkingEndTime = working.getLast().end;
+            }
+        }
+        return new Pair<>(working, multiTasking);
+
+
+    }
 
 
 }
