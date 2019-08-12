64d7c3b85fe469198e3cc2f4fa87cf6bbf6dba8d
Nitin Gupta <iit.gupta2010@gmail.com>
2019-08-13 00:12:41 +0530

    MergeKSortedIterators

--- Java/companyWise/Amazon/MergeKSortedIterators.java
+++ Java/companyWise/Amazon/MergeKSortedIterators.java
@@ -1,6 +1,7 @@
 package Java.companyWise.Amazon;
 
 import Java.LeetCode.iterators.PeekingIterators;
+import javafx.util.Pair;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -50,43 +51,135 @@ public class MergeKSortedIterators {
 
     private static void test(List<List<Integer>> asList) {
 
-
         priorityQueue(asList);
+        priorityQueuePeekIterator(asList);
         priorityQueueCache(asList);
     }
 
-
     private static void priorityQueue(List<List<Integer>> asList) {
 
-        System.out.println("\n\nPriorityQueue-> Input :" + asList);
-        MergeKSortedIteratorsUsingPriorityQueue.MergingIterator v1Iterator = new MergeKSortedIteratorsUsingPriorityQueue.MergingIterator(asList.stream().map(l -> l.iterator()).collect(Collectors.toList()));
+        System.out.println("\n\nUsing Priority Queue-> Input :" + asList);
+        MergeKSortedIteratorsUsingPriorityQueueOnly.MergingIterator iterator = new MergeKSortedIteratorsUsingPriorityQueueOnly.MergingIterator(asList.stream().map(l -> l.iterator()).collect(Collectors.toList()));
 
 
-        while (v1Iterator.hasNext())
-            System.out.print(v1Iterator.next() + " ");
+        while (iterator.hasNext())
+            System.out.print(iterator.next() + " ");
+
+    }
+
+
+    private static void priorityQueuePeekIterator(List<List<Integer>> asList) {
+
+        System.out.println("\n\nUsing Peek Iterator-> Input :" + asList);
+        MergeKSortedIteratorsUsingPriorityQueueAndPeekIterator.MergingIterator iterator = new MergeKSortedIteratorsUsingPriorityQueueAndPeekIterator.MergingIterator(asList.stream().map(l -> l.iterator()).collect(Collectors.toList()));
+
+
+        while (iterator.hasNext())
+            System.out.print(iterator.next() + " ");
 
     }
 
     private static void priorityQueueCache(List<List<Integer>> asList) {
 
-        System.out.println("\n\nPriorityQueue cache-> Input :" + asList);
-        MergeKSortedIteratorsUsingPriorityQueueCached.MergingIterator v1Iterator = new MergeKSortedIteratorsUsingPriorityQueueCached.MergingIterator(asList.stream().map(l -> l.iterator()).collect(Collectors.toList()));
+        System.out.println("\n\nUsing cache-> Input :" + asList);
+        MergeKSortedIteratorsUsingPriorityQueueCached.MergingIterator iterator = new MergeKSortedIteratorsUsingPriorityQueueCached.MergingIterator(asList.stream().map(l -> l.iterator()).collect(Collectors.toList()));
 
 
-        while (v1Iterator.hasNext())
-            System.out.print(v1Iterator.next() + " ");
+        while (iterator.hasNext())
+            System.out.print(iterator.next() + " ");
 
     }
 
 
 }
 
+
+/**
+ * This approach take the advantage of keeping iterator and the next value for this iterator in
+ * Priority queue it self.
+ * <p>
+ * Algorithm:
+ * 1. Initially we'll push all non-empty iterator first value and iterator (after taking first value) in priority queue as pair
+ * 2. For next, we'll poll the top Pair and gets its value, then check does this "poll" iterator has more entry to give.
+ * If yes, fetch next entry form this iterator, put a new entry in Priority Queue as this iterator (after taking  value) as key and fetched value as value
+ * 3. HasNext is simply can test does priority queue is empty or not.
+ * <p>
+ * <p>
+ * HashNext: O(1) where size is Length of iterators list
+ * next: O(1)
+ */
+class MergeKSortedIteratorsUsingPriorityQueueOnly {
+
+
+    /**
+     * Using Sorted list like data structure;
+     * Using Priority Queue
+     */
+    static class MergingIterator implements Iterator<Integer> {
+
+        private final PriorityQueue<Pair<Iterator<Integer>, Integer>> priorityQueue;
+
+
+        public MergingIterator(List<Iterator<Integer>> iterators) {
+
+            priorityQueue = new PriorityQueue<>(Comparator.comparing(Pair::getValue));
+            init(iterators);
+        }
+
+        /**
+         * O(size * log(size)) where size is Length of iterators list
+         */
+        private final void init(List<Iterator<Integer>> iterators) {
+            for (Iterator<Integer> iterator : iterators) {
+
+                if (iterator.hasNext()) {
+
+                    Integer value = iterator.next();
+                    priorityQueue.offer(new Pair<>(iterator, value));
+                }
+            }
+
+        }
+
+        /**
+         * O(1) where size is Length of iterators list
+         */
+        public boolean hasNext() {
+            return !priorityQueue.isEmpty();
+
+
+        }
+
+        /**
+         * O(log(size))
+         */
+        public Integer next() {
+
+            Pair<Iterator<Integer>, Integer> poll = priorityQueue.poll();
+
+            Integer toReturn = poll.getValue();
+            if (poll.getKey().hasNext()) {
+                Integer next = poll.getKey().next();
+                priorityQueue.offer(new Pair<>(poll.getKey(), next));
+            }
+
+            return toReturn;
+
+        }
+    }
+}
+
 /**
  * HashNext: O(size) where size is Length of iterators list
  * next: O(1)
  */
-class MergeKSortedIteratorsUsingPriorityQueue {
+class MergeKSortedIteratorsUsingPriorityQueueAndPeekIterator {
 
+    /**
+     * {@link PeekingIterators}
+     *
+     * @param <E>
+     */
     private static class PeekingIterator<E> implements Iterator<E> {
 
         private E lastElement = null;
@@ -184,7 +277,7 @@ class MergeKSortedIteratorsUsingPriorityQueue {
 
 /**
  * This kind of implementation is not really a iterator. As it cache the whole data.
- *
+ * <p>
  * HashNext: O(1) where size is Length of iterators list
  * next: O(1)
  */
@@ -236,3 +329,4 @@ class MergeKSortedIteratorsUsingPriorityQueueCached {
         }
     }
 }
+
