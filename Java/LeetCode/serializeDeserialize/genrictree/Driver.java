869145a76be1e0f33fcb0cd5d776f519578f8bfd
Nitin Gupta <iit.gupta2010@gmail.com>
2019-08-22 15:46:50 +0530

    Serialize BST, nArray Tree

--- Java/LeetCode/serializeDeserialize/genrictree/Driver.java
+++ Java/LeetCode/serializeDeserialize/genrictree/Driver.java
@@ -45,7 +45,14 @@ public class Driver {
 
     public static void main(String args[]) {
 
-        NArrayTreeNode root = getTree();
+        test(getTree());
+        test(getTree2());
+
+
+    }
+
+    private static void test(NArrayTreeNode root) {
+        System.out.println("\n Running...\n");
         System.out.println(levelOrder(root));
 
         ISerializeDeserializeNArrayTree nArrayTree = new SerializeDeserializeNAryTree();
@@ -53,7 +60,6 @@ public class Driver {
         System.out.println("serialized : " + serialized);
         NArrayTreeNode de = nArrayTree.deserialize(serialized);
         System.out.println(levelOrder(de));
-
 
     }
 
@@ -69,6 +75,23 @@ public class Driver {
 
         NArrayTreeNode ttt = root.children.get(2);
         ttt.children = Arrays.asList(new NArrayTreeNode(21), new NArrayTreeNode(24), new NArrayTreeNode(25));
+
+
+        return root;
+    }
+
+    private static NArrayTreeNode getTree2() {
+        NArrayTreeNode root = new NArrayTreeNode();
+        root.val = 10;
+        root.children = Arrays.asList(new NArrayTreeNode(2), new NArrayTreeNode(20), new NArrayTreeNode(12));
+        NArrayTreeNode t = root.children.get(0);
+        t.children = Arrays.asList(new NArrayTreeNode(1));
+
+        NArrayTreeNode tt = root.children.get(1);
+        tt.children = Arrays.asList(new NArrayTreeNode(8));
+
+        NArrayTreeNode ttt = root.children.get(2);
+        ttt.children = Arrays.asList(new NArrayTreeNode(21));
 
 
         return root;
