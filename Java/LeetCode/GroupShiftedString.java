f6ddc9fdf5c30953f672261c6f99edf074b90508
Nitin Gupta <iit.gupta2010@gmail.com>
2019-09-02 14:10:15 +0530

    minor change

--- Java/LeetCode/GroupShiftedString.java
+++ Java/LeetCode/GroupShiftedString.java
@@ -1,11 +1,30 @@
-package Java;
+package Java.LeetCode;
 
 import java.util.*;
 
 /**
  * Author: Nitin Gupta(nitin.gupta@walmart.com)
  * Date: 2019-08-27
- * Description: https://www.geeksforgeeks.org/group-shifted-string/
+ * Description:
+ * https://leetcode.com/problems/group-shifted-strings/
+ * * 249.Group Shifted Strings
+ * * Given a string, we can "shift" each of its letter to its successive letter, for example: "abc" -> "bcd". We can keep "shifting" which forms the sequence:
+ * * <p>
+ * * "abc" -> "bcd" -> ... -> "xyz"
+ * * Given a list of strings which contains only lowercase alphabets, group all strings that belong to the same shifting sequence.
+ * * <p>
+ * * Example:
+ * * <p>
+ * * Input: ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"],
+ * * Output:
+ * * [
+ * * ["abc","bcd","xyz"],
+ * * ["az","ba"],
+ * * ["acef"],
+ * * ["a","z"]
+ * * ]
+ * <p>
+ * https://www.geeksforgeeks.org/group-shifted-string/
  * Given an array of strings (all lowercase letters), the task is to group them in such a way that all strings in a group are shifted versions of each other. Two string S and T are called shifted if,
  * <p>
  * S.length = T.length
@@ -27,6 +46,8 @@ import java.util.*;
  * so we need to change our hash function; Since the hash function in above will generate
  * same hash for
  * meoqs [12345] and bedfh [12345] But they are not shifted string
+ * <p>
+ * <p>
  */
 public class GroupShiftedString {
 
