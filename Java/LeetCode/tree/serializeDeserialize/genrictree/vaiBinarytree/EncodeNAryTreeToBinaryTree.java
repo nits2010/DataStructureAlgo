956a72df738946b4674faa9dcce227c6c245c675
Nitin Gupta <iit.gupta2010@gmail.com>
2019-09-27 13:37:43 +0530

    encode n array tree to binary tree and decode-a

---
+++ Java/LeetCode/tree/serializeDeserialize/genrictree/vaiBinarytree/EncodeNAryTreeToBinaryTree.java
@@ -0,0 +1,9 @@
+package Java.LeetCode.tree.serializeDeserialize.genrictree.vaiBinarytree;
+
+/**
+ * Author: Nitin Gupta
+ * Date: 27/09/19
+ * Description:
+ */
+public class EncodeNAryTreeToBinaryTree {
+}
