b74e6f4d4a931442e3c6cccf30edbeed54d8b0bd
nits2010 <iit.gupta2010@gmail.com>
2024-08-18 20:51:48 +0530

    minor

--- src/main/java/DataStructureAlgo/Java/nonleetcode/Tree/traversal/TreeTraversalRecursive.java
+++ src/main/java/DataStructureAlgo/Java/nonleetcode/Tree/traversal/TreeTraversalRecursive.java
@@ -2,7 +2,7 @@ package DataStructureAlgo.Java.nonleetcode.Tree.traversal;
 
 import DataStructureAlgo.Java.helpers.templates.NArrayTreeNode;
 
-import  DataStructureAlgo.Java.nonleetcode.Tree.TreeNode;
+import DataStructureAlgo.Java.nonleetcode.Tree.TreeNode;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -22,11 +22,11 @@ import java.util.Queue;
  * Binary Tree Inorder Traversal <a href="https://leetcode.com/problems/binary-tree-inorder-traversal/description/">...</a>
  * Binary Tree Preorder Traversal <a href="https://leetcode.com/problems/binary-tree-preorder-traversal/description/">...</a>
  * Binary Tree level order traversal <a href="https://leetcode.com/problems/binary-tree-level-order-traversal/description/">...</a>
- *
+ * <p>
  * N-ary Tree Preorder Traversal <a href="https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/">...</a>
  * N-ary Tree Postorder Traversal <a href="https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/">...</a>
- *
- *
+ * <p>
+ * <p>
  * File reference
  * -----------
  * Duplicate {@link}
@@ -35,13 +35,12 @@ import java.util.Queue;
  * <p>
  * Tags
  * -----
+ *
  * @easy
  * @Stack
  * @Tree
  * @Depth-FirstSearch
- * @BinaryTree
- *
- * Company Tags
+ * @BinaryTree Company Tags
  * -----
  * @Adobe
  * @Google
@@ -52,12 +51,7 @@ import java.util.Queue;
  * @Apple
  * @LinkedIn
  * @Bloomberg
- *
-
- *
- *
- * @Editorial
- * <a href="https://leetcode.com/problems/binary-tree-preorder-traversal/solutions/5620461/recursive-easy-iterative-with-stack-iterative-without-stack-o-n-o-1">...</a>
+ * @Editorial <a href="https://leetcode.com/problems/binary-tree-preorder-traversal/solutions/5620461/recursive-easy-iterative-with-stack-iterative-without-stack-o-n-o-1">...</a>
  * <a href="https://leetcode.com/problems/binary-tree-inorder-traversal/solutions/5620435/recursive-easy-iterative-with-stack-iterative-without-stack-o-n-o-1">...</a>
  */
 public class TreeTraversalRecursive {
@@ -242,15 +236,15 @@ public class TreeTraversalRecursive {
         while (!queue.isEmpty()) {
             TreeNode temp = queue.poll();
 
-            if(temp == null){
-                if(queue.isEmpty())
+            if (temp == null) {
+                if (queue.isEmpty())
                     return levelOrder;
                 else
                     queue.offer(null);
                 List<Integer> nextChildren = new LinkedList<>();
                 levelOrder.add(nextChildren);
 
-            }else {
+            } else {
                 if (levelOrder.isEmpty()) {
                     levelOrder.add(new LinkedList<>());
                 }
@@ -258,9 +252,9 @@ public class TreeTraversalRecursive {
                 List<Integer> children = levelOrder.get(levelOrder.size() - 1);
                 children.add((Integer) temp.getData());
 
-                if(temp.getLeft()!=null)
+                if (temp.getLeft() != null)
                     queue.offer(temp.getLeft());
-                if(temp.getRight()!=null)
+                if (temp.getRight() != null)
                     queue.offer(temp.getRight());
             }
 
@@ -268,9 +262,6 @@ public class TreeTraversalRecursive {
         }
         return levelOrder;
     }
-
-
-
 
 
     public static List<Integer> levelOrder(DataStructureAlgo.Java.helpers.templates.TreeNode root) {
@@ -311,25 +302,25 @@ public class TreeTraversalRecursive {
         while (!queue.isEmpty()) {
             DataStructureAlgo.Java.helpers.templates.TreeNode temp = queue.poll();
 
-            if(temp == null){
-                if(queue.isEmpty())
+            if (temp == null) {
+                if (queue.isEmpty())
                     return levelOrder;
                 else
                     queue.offer(null);
                 List<Integer> nextChildren = new LinkedList<>();
                 levelOrder.add(nextChildren);
 
-            }else {
+            } else {
                 if (levelOrder.isEmpty()) {
                     levelOrder.add(new LinkedList<>());
                 }
 
                 List<Integer> children = levelOrder.get(levelOrder.size() - 1);
-                children.add((Integer) temp.val);
+                children.add(temp.val);
 
-                if(temp.left!=null)
+                if (temp.left != null)
                     queue.offer(temp.left);
-                if(temp.right!=null)
+                if (temp.right != null)
                     queue.offer(temp.right);
             }
 
@@ -339,21 +330,50 @@ public class TreeTraversalRecursive {
     }
 
 
+    private List<List<Integer>> levelOrderSimple(DataStructureAlgo.Java.helpers.templates.TreeNode root) {
+        List<List<Integer>> levelOrder = new LinkedList<>();
+
+        if (null == root) return levelOrder;
+
+        Queue<DataStructureAlgo.Java.helpers.templates.TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            List<Integer> level = new LinkedList<>();
+            while (size > 0) {
+                DataStructureAlgo.Java.helpers.templates.TreeNode current = queue.poll();
+                level.add(current.val);
+
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+
+                size--;
+
+            }
+            levelOrder.add(level);
+        }
+
+        return levelOrder;
+    }
+
+
     /****************** PreOrder - public static  implementation Traversal For N-ary Tree *******************/
     public static List<Integer> preOrder(NArrayTreeNode root) {
         List<Integer> preOrder = new LinkedList<>();
-        if(root == null)
+        if (root == null)
             return preOrder;
         preOrderUtil(root, preOrder);
         return preOrder;
     }
 
     private static void preOrderUtil(NArrayTreeNode root, List<Integer> preorder) {
-        if(root == null)
+        if (root == null)
             return;
 
         preorder.add(root.val);
-        for(NArrayTreeNode child : root.children)
+        for (NArrayTreeNode child : root.children)
             preOrderUtil(child, preorder);
     }
 
@@ -361,17 +381,17 @@ public class TreeTraversalRecursive {
 
     public static List<Integer> postOrder(NArrayTreeNode root) {
         List<Integer> postOrder = new LinkedList<>();
-        if(root == null)
+        if (root == null)
             return postOrder;
         postOrderUtil(root, postOrder);
         return postOrder;
     }
 
     private static void postOrderUtil(NArrayTreeNode root, List<Integer> postOrder) {
-        if(root == null)
+        if (root == null)
             return;
 
-        for(NArrayTreeNode child : root.children)
+        for (NArrayTreeNode child : root.children)
             postOrderUtil(child, postOrder);
         postOrder.add(root.val);
     }
@@ -380,27 +400,25 @@ public class TreeTraversalRecursive {
 
     public static List<List<Integer>> levelOrder(NArrayTreeNode root) {
         List<List<Integer>> levelOrder = new ArrayList<>();
-        if(root == null)
+        if (root == null)
             return levelOrder;
 
-        levelOrderUtil(root,levelOrder, 0);
+        levelOrderUtil(root, levelOrder, 0);
         return levelOrder;
     }
 
     private static void levelOrderUtil(NArrayTreeNode root, List<List<Integer>> levelOrder, int level) {
 
         //a new level needs to be start
-        if(level == levelOrder.size())
+        if (level == levelOrder.size())
             levelOrder.add(new LinkedList<>());
 
         levelOrder.get(level).add(root.val);
 
-        for(NArrayTreeNode child : root.children)
-            levelOrderUtil(child, levelOrder, level+1);
+        for (NArrayTreeNode child : root.children)
+            levelOrderUtil(child, levelOrder, level + 1);
 
     }
-
-
 
 
 }
