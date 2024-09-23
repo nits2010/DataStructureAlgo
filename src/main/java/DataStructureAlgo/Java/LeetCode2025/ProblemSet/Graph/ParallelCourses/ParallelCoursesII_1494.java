a0f6e1f7e8e55aa8d30bce37c0a87f33734e439a
ngupta14 <nitin_gupta2@intuit.com>
2024-09-23 17:05:05 +0530

    minor

--- src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Graph/ParallelCourses/ParallelCoursesII_1494.java
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Graph/ParallelCourses/ParallelCoursesII_1494.java
@@ -11,7 +11,9 @@ import java.util.stream.IntStream;
  * Question Category: 1494. Parallel Courses II
  * Description:https://leetcode.com/problems/parallel-courses-ii/description/
  *
- *You are given an integer n, which indicates that there are n courses labeled from 1 to n. You are also given an array relations where relations[i] = [prevCoursei, nextCoursei], representing a prerequisite relationship between course prevCoursei and course nextCoursei: course prevCoursei has to be taken before course nextCoursei. Also, you are given the integer k.
+ *You are given an integer n, which indicates that there are n courses labeled from 1 to n.
+ * You are also given an array relations where relations[i] = [prevCoursei, nextCoursei], representing a prerequisite relationship between course prevCoursei and course nextCoursei: course prevCoursei
+ * has to be taken before course nextCoursei. Also, you are given the integer k.
  *
  * In one semester, you can take at most k courses as long as you have taken all the prerequisites in the previous semesters for the courses you are taking.
  *
