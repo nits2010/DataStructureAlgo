5c569f73a9d936d92cf0c2157caf6548d6a03ecd
nits2010 <iit.gupta2010@gmail.com>
2024-10-23 18:21:32 +0530

    CousinsInBinaryTreeII_2641

--- src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Trees/_2641/CousinsInBinaryTreeII_2641.java
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Trees/_2641/CousinsInBinaryTreeII_2641.java
@@ -70,7 +70,7 @@ import java.util.*;
  * -----
  * @Editorial <p><p>
  * -----
- * @OptimalSoltuion
+ * @OptimalSoltuion {@link BFS.Solution_OnePassBFS}
  */
 public class CousinsInBinaryTreeII_2641 {
 
@@ -93,13 +93,33 @@ public class CousinsInBinaryTreeII_2641 {
         List<Integer> outputLevelOrder;
         boolean pass, finalPass = true;
 
-        Solution_TwoPassBFS solutionTwoPassBFS = new Solution_TwoPassBFS();
+        BFS.Solution_TwoPassBFS solutionTwoPassBFS = new BFS.Solution_TwoPassBFS();
         output = solutionTwoPassBFS.replaceValueInTree(TreeBuilder.buildTreeFromLevelOrder(input));
         outputLevelOrder = TreeTraversalRecursive.levelOrderWithNull(output);
         pass = Arrays.deepEquals(expected, outputLevelOrder.toArray());
         finalPass &= pass;
         System.out.println("Two Pass BFS : " + outputLevelOrder + " Pass : " + (pass ? "PASS" : "FAIL"));
 
+        DFS.Solution_TwoPassDFS solutionTwoPassDFS = new DFS.Solution_TwoPassDFS();
+        output = solutionTwoPassDFS.replaceValueInTree(TreeBuilder.buildTreeFromLevelOrder(input));
+        outputLevelOrder = TreeTraversalRecursive.levelOrderWithNull(output);
+        pass = Arrays.deepEquals(expected, outputLevelOrder.toArray());
+        finalPass &= pass;
+        System.out.println("Two Pass DFS : " + outputLevelOrder + " Pass : " + (pass ? "PASS" : "FAIL"));
+
+        BFS.Solution_OnePassBFS solutionOnePassBFS = new BFS.Solution_OnePassBFS();
+        output = solutionOnePassBFS.replaceValueInTree(TreeBuilder.buildTreeFromLevelOrder(input));
+        outputLevelOrder = TreeTraversalRecursive.levelOrderWithNull(output);
+        pass = Arrays.deepEquals(expected, outputLevelOrder.toArray());
+        finalPass &= pass;
+        System.out.println("One Pass BFS : " + outputLevelOrder + " Pass : " + (pass ? "PASS" : "FAIL"));
+
+        DFS.Solution_OnePassDFS solutionOnePassDFS = new DFS.Solution_OnePassDFS();
+        output = solutionOnePassDFS.replaceValueInTree(TreeBuilder.buildTreeFromLevelOrder(input));
+        outputLevelOrder = TreeTraversalRecursive.levelOrderWithNull(output);
+        pass = Arrays.deepEquals(expected, outputLevelOrder.toArray());
+        finalPass &= pass;
+        System.out.println("One Pass DFS : " + outputLevelOrder + " Pass : " + (pass ? "PASS" : "FAIL"));
 
         System.out.println("-------------------------------------------------");
 
@@ -107,75 +127,259 @@ public class CousinsInBinaryTreeII_2641 {
 
     }
 
+    static class BFS {
+        static class Solution_TwoPassBFS {
+            public TreeNode replaceValueInTree(TreeNode root) {
+                Queue<TreeNode> q = new LinkedList<>();
+                q.offer(root);
 
-    static class Solution_TwoPassBFS {
-        public TreeNode replaceValueInTree(TreeNode root) {
-            Queue<TreeNode> q = new LinkedList<>();
-            q.offer(root);
+                List<Integer> levelSum = new ArrayList<>();
 
-            List<Integer> levelSum = new ArrayList<>();
+                //acquire sum
+                while (!q.isEmpty()) {
+                    int size = q.size();
 
-            //acquire sum
-            while (!q.isEmpty()) {
-                int size = q.size();
+                    int sum = 0;
+                    for (int i = 0; i < size; i++) {
+                        TreeNode node = q.poll();
 
-                int sum = 0;
-                for (int i = 0; i < size; i++) {
-                    TreeNode node = q.poll();
+                        if (node.left != null)
+                            q.offer(node.left);
+                        if (node.right != null)
+                            q.offer(node.right);
 
-                    if (node.left != null)
-                        q.offer(node.left);
-                    if (node.right != null)
-                        q.offer(node.right);
+                        sum += node.val;
+                    }
 
-                    sum += node.val;
+                    levelSum.add(sum);
                 }
 
-                levelSum.add(sum);
-            }
+                //distribute sum
+                q.offer(root);
+                int level = 1;
+                while (!q.isEmpty()) {
+                    int size = q.size();
 
-            //distribute sum
-            q.offer(root);
-            int level = 1;
-            while (!q.isEmpty()) {
-                int size = q.size();
+                    for (int i = 0; i < size; i++) {
+                        TreeNode node = q.poll();
 
-                for (int i = 0; i < size; i++) {
-                    TreeNode node = q.poll();
+                        int sibSum = 0;
+                        //acquire sibsum
+                        if (node.left != null) {
+                            sibSum += node.left.val;
+                            q.offer(node.left);
+                        }
+                        if (node.right != null) {
+                            sibSum += node.right.val;
+                            q.offer(node.right);
+                        }
 
-                    int sibSum = 0;
-                    //accuire sibsum
-                    if (node.left != null) {
-                        sibSum += node.left.val;
-                        q.offer(node.left);
+                        //replace sibSum
+                        if (node.left != null) {
+                            node.left.val = levelSum.get(level) - sibSum;
+                        }
+
+                        if (node.right != null) {
+                            node.right.val = levelSum.get(level) - sibSum;
+
+                        }
                     }
-                    if (node.right != null) {
-                        sibSum += node.right.val;
-                        q.offer(node.right);
-                    }
 
-                    //replace sibSum
-                    if (node.left != null) {
-                        node.left.val = levelSum.get(level) - sibSum;
-                    }
 
-                    if (node.right != null) {
-                        node.right.val = levelSum.get(level) - sibSum;
-
-                    }
+                    level++;
                 }
 
+                root.val = 0;
 
-                level++;
+                return root;
+
+
             }
-
-            root.val = 0;
-
-            return root;
 
 
         }
 
+        /**
+         * In Two pass BFS, we first acquire the sum and then distribute the sum at each node in level order. More precisely, while distributing, we are distributing a sum at the next level, as we already
+         * know the sum at the next level, previously computed. Now, what if we know the sum of next level and sum of current level?
+         * While doing BFS, we can always calculate the current Level sum (root.val) and the next level sum(root.left.val + root.right.val) and post every level, the nextLevelSUm will be our currentLevelSum (swap).
+         * <p>
+         * Now the challenge is, how we can replace the current node with its sibling sum, while we have the partial current level sum (root.val)?
+         * To avoid such a situation, we can cache the sibling sum at each node and use it to replace the current node while processing the next level.
+         */
+        static class Solution_OnePassBFS {
+            public TreeNode replaceValueInTree(TreeNode root) {
+                Queue<TreeNode> q = new LinkedList<>();
+                q.offer(root);
 
+                //the current level which is under consideration
+                int currentLevelSum = root.val;
+
+                //acquire sum
+                while (!q.isEmpty()) {
+                    int size = q.size();
+
+                    //the next level sum of the current node
+                    int nextLevelSum = 0;
+
+                    for (int i = 0; i < size; i++) {
+                        TreeNode node = q.poll();
+
+                        //reduce node value by current levelSum, since node.val value already has the total sibling sum
+                        node.val = currentLevelSum - node.val;
+
+                        int sibSum = 0;
+                        if (node.left != null) {
+                            nextLevelSum += node.left.val;
+                            sibSum += node.left.val;
+                        }
+
+                        if (node.right != null) {
+                            nextLevelSum += node.right.val;
+                            sibSum += node.right.val;
+                        }
+
+                        if (node.left != null) {
+                            node.left.val = sibSum; //cache the sibSum at each node so that we can replace it
+                            q.offer(node.left);
+                        }
+
+                        if (node.right != null) {
+                            node.right.val = sibSum;
+                            q.offer(node.right);
+                        }
+
+
+                    }
+
+                    currentLevelSum = nextLevelSum;
+
+
+                }
+
+
+                root.val = 0;
+
+                return root;
+
+
+            }
+
+
+        }
+    }
+
+    static class DFS {
+
+
+        static class Solution_TwoPassDFS {
+            public TreeNode replaceValueInTree(TreeNode root) {
+                List<Integer> levelSum = new ArrayList<>();
+                dfsAcquireSum(root, levelSum, 0);
+                dfsDistributeSum(root, levelSum, 1);
+                root.val = 0;
+                return root;
+            }
+
+            private void dfsDistributeSum(TreeNode root, List<Integer> levelSum, int level) {
+                if (root == null) return;
+
+                int sibSum = 0;
+
+                if (root.left != null) {
+                    sibSum += root.left.val;
+                }
+
+                if (root.right != null) {
+                    sibSum += root.right.val;
+                }
+
+                if (root.left != null) {
+                    root.left.val = levelSum.get(level) - sibSum;
+                }
+
+                if (root.right != null) {
+                    root.right.val = levelSum.get(level) - sibSum;
+                }
+                dfsDistributeSum(root.left, levelSum, level + 1);
+                dfsDistributeSum(root.right, levelSum, level + 1);
+
+            }
+
+            private void dfsAcquireSum(TreeNode root, List<Integer> levelSum, int level) {
+                if (root == null) return;
+
+                if (levelSum.size() == level)
+                    levelSum.add(0);
+
+                levelSum.set(level, levelSum.get(level) + root.val);
+                dfsAcquireSum(root.left, levelSum, level + 1);
+                dfsAcquireSum(root.right, levelSum, level + 1);
+            }
+        }
+
+        static class Solution_OnePassDFS {
+            public TreeNode replaceValueInTree(TreeNode root) {
+
+                //this list hols node of current level under processing
+                List<TreeNode> levelNodes = new ArrayList<>();
+                levelNodes.add(root);
+                dfsAcquireDistributeSum(levelNodes, 0);
+                root.val = 0;
+                return root;
+            }
+
+            private void dfsAcquireDistributeSum(List<TreeNode> levelNodes, int level) {
+
+                //there are no more nodes  to process
+                if (levelNodes.isEmpty())
+                    return;
+
+                //process the current level and calculate the levelSum
+                int currentLevelSum = 0;
+
+                //cache the next level nodes to process later
+                List<TreeNode> nextLevelNodes = new ArrayList<>();
+
+                for (TreeNode node : levelNodes) {
+                    if (node.left != null) {
+                        currentLevelSum += node.left.val;
+                        nextLevelNodes.add(node.left);
+                    }
+                    if (node.right != null) {
+                        currentLevelSum += node.right.val;
+                        nextLevelNodes.add(node.right);
+                    }
+                }
+
+
+                //update the current level nodes with sibSum
+                for (TreeNode node : levelNodes) {
+                    int sibSum = 0;
+
+                    if (node.left != null) {
+                        sibSum += node.left.val;
+                    }
+
+                    if (node.right != null) {
+                        sibSum += node.right.val;
+                    }
+
+                    if (node.left != null) {
+                        node.left.val = currentLevelSum - sibSum;
+
+                    }
+
+                    if (node.right != null) {
+                        node.right.val = currentLevelSum - sibSum;
+
+                    }
+                }
+
+                dfsAcquireDistributeSum(nextLevelNodes, level + 1);
+            }
+
+
+        }
     }
 }
