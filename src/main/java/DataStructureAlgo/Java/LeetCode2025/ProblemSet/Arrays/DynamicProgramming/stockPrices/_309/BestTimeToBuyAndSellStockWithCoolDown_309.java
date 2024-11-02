8aa48b49e23cb061ebd1056a1490bd9879d1a539
nits2010 <iit.gupta2010@gmail.com>
2024-11-02 13:51:27 +0530

    BestTimeToBuyAndSellStockWithCoolDown_309

--- src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Arrays/DynamicProgramming/stockPrices/_309/BestTimeToBuyAndSellStockWithCoolDown_309.java
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Arrays/DynamicProgramming/stockPrices/_309/BestTimeToBuyAndSellStockWithCoolDown_309.java
@@ -1,5 +1,9 @@
 package DataStructureAlgo.Java.LeetCode2025.ProblemSet.Arrays.DynamicProgramming.stockPrices._309;
 
+import DataStructureAlgo.Java.helpers.CommonMethods;
+
+import java.util.*;
+
 /**
  * Author: Nitin Gupta
  * Date: 11/1/2024
@@ -52,4 +56,224 @@ package DataStructureAlgo.Java.LeetCode2025.ProblemSet.Arrays.DynamicProgramming
  * @OptimalSoltuion
  */
 public class BestTimeToBuyAndSellStockWithCoolDown_309 {
+
+    public static void main(String[] args) {
+        List<Boolean> tests = new ArrayList<>();
+        tests.add(test(new int[]{1, 2, 3, 0, 2}, 3));
+        tests.add(test(new int[]{1}, 0));
+        tests.add(test(new int[]{2, 1, 4}, 3));
+        tests.add(test(new int[]{1, 2, 4}, 3));
+
+        CommonMethods.printAllTestOutCome(tests);
+    }
+
+
+    private static boolean test(int[] prices, int expected) {
+        CommonMethods.printTestOutcome(new String[]{"Prices", "Expected"}, true, prices, expected);
+
+        int output;
+        boolean pass, finalPass = true;
+
+        Recursion.Solution_Recursion recursion = new Recursion.Solution_Recursion();
+        output = recursion.maxProfit(prices);
+        pass = output == expected;
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"Recursion", "Pass"}, false, output, pass ? "Pass" : "Fail");
+
+
+        DynamicProgramming.Solution_TopDownMemoization topDown = new DynamicProgramming.Solution_TopDownMemoization();
+        output = topDown.maxProfit(prices);
+        pass = output == expected;
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"TopDownMemoization", "Pass"}, false, output, pass ? "Pass" : "Fail");
+
+
+        DynamicProgramming.Solution_BottomUpTabulation bottomUp = new DynamicProgramming.Solution_BottomUpTabulation();
+        output = bottomUp.maxProfit(prices);
+        pass = output == expected;
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"BottomUp", "Pass"}, false, output, pass ? "Pass" : "Fail");
+
+        DynamicProgramming.Solution_BottomUpTabulation_Optimized bottomUpTabulationOptimized = new DynamicProgramming.Solution_BottomUpTabulation_Optimized();
+        output = bottomUpTabulationOptimized.maxProfit(prices);
+        pass = output == expected;
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"BottomUp-Optimized", "Pass"}, false, output, pass ? "Pass" : "Fail");
+
+        DynamicProgramming.Solution_BottomUpTabulation_Optimized_SpaceOptimized bottomUpTabulationOptimizedSpaceOptimized = new DynamicProgramming.Solution_BottomUpTabulation_Optimized_SpaceOptimized();
+        output = bottomUpTabulationOptimizedSpaceOptimized.maxProfit(prices);
+        pass = output == expected;
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"BottomUp-Optimized", "Pass"}, false, output, pass ? "Pass" : "Fail");
+
+
+
+
+
+        return finalPass;
+
+    }
+
+    static class Recursion {
+        static class Solution_Recursion {
+            final int buying = 1;
+            final int selling = 0;
+
+            public int maxProfit(int[] prices) {
+                return maxProfit(prices, 0, prices.length, 1);
+            }
+
+            private int maxProfit(int[] prices, int i, int n, int buy) {
+
+                if (i >= n)
+                    return 0;
+
+
+                int profit;
+                if (buy == 1) {
+                    profit = Math.max(maxProfit(prices, i + 1, n, selling) - prices[i], // sell at i+1, buy so prices[i] will be deducted)
+                            maxProfit(prices, i + 1, n, buying)); // don't buy, do cool down
+
+                } else {
+                    // its selling day
+                    profit = Math.max(maxProfit(prices, i + 2, n, buying) + prices[i], // sold to gain prices[i], i+2 since i+1 is cool down
+                            maxProfit(prices, i + 1, n, selling)); // don't sell, do cool down
+                }
+                return profit;
+            }
+        }
+    }
+
+    static class DynamicProgramming {
+        static class Solution_TopDownMemoization {
+            final int buying = 1;
+            final int selling = 0;
+
+            public int maxProfit(int[] prices) {
+                int n = prices.length;
+                Integer[][] dp = new Integer[n][2]; // n days, with two possibilities buy or sell
+                return maxProfit(prices, 0, n, 1, dp);
+            }
+
+            private int maxProfit(int[] prices, int i, int n, int buy, Integer[][] dp) {
+                // System.out.println(buy);
+
+                if (i >= n)
+                    return 0;
+
+                if (dp[i][buy] != null)
+                    return dp[i][buy];
+
+                int profit;
+                if (buy == 1) {
+                    profit = Math.max(maxProfit(prices, i + 1, n, selling, dp) - prices[i], // sell at i+1, buy so prices[i] will be deducted)
+                            maxProfit(prices, i + 1, n, buying, dp)); // don't buy, do cool-down
+
+                } else {
+                    // its selling day
+                    profit = Math.max(maxProfit(prices, i + 2, n, buying, dp) + prices[i], // sold to gain prices[i], i+2 since i+1 is cool-down
+                            maxProfit(prices, i + 1, n, selling, dp)); // don't sell today, do cool-down
+                }
+                return dp[i][buy] = profit;
+            }
+        }
+
+        static class Solution_BottomUpTabulation {
+
+            public int maxProfit(int[] prices) {
+                int n = prices.length;
+                int[][] dp = new int[n + 2][2]; // n days, with two possibilities buy or sell
+
+                //dp[i][1] defines the profit gained by buying on day i
+                //dp[i][0] defines the profit gained by selling on day i
+                // dp[i][buy] = Math.max (
+                //                      Max ( <buy on ith day, sell on i+1 day> - prices[i] , cool-down on i+1 day> ),
+                //                      Max ( <sell on ith day and do cool down on i+1 day> + prices[i], <cool-down on i+1 day> )
+                // dp[i][buy] = Math.max ( Math.max(dp[i+1][0] - prices[i], dp[i+1][1]), Math.max( dp[i+2][1] + prices[i], dp[i+1][0] ) )
+                // since cool-down is common, we can extract it
+                // dp[i][buy] = Max ( Max(dp[i+1][0] - prices[i] , dp[i+2][1] + prices[i]), dp[i+1][0] )
+
+                for (int i = n - 1; i >= 0; i--) {
+                    for (int buy = 1; buy >= 0; buy--) {
+
+                        if (buy == 1) {
+                            dp[i][buy] = Math.max(dp[i + 1][0] - prices[i], dp[i + 1][1]);
+                        } else {
+                            dp[i][buy] = Math.max(dp[i + 2][1] + prices[i], dp[i + 1][0]);
+                        }
+                    }
+                }
+                return dp[0][1];
+
+            }
+
+
+        }
+
+        static class Solution_BottomUpTabulation_Optimized {
+
+            public int maxProfit(int[] prices) {
+                int n = prices.length;
+                int[][] dp = new int[n + 2][2]; // n days, with two possibilities buy or sell
+
+                //dp[i][1] defines the profit gained by buying on day i
+                //dp[i][0] defines the profit gained by selling on day i
+                // dp[i][buy] = Math.max (
+                //                      Max ( <buy on ith day, sell on i+1 day> - prices[i] , cool-down on i+1 day> ),
+                //                      Max ( <sell on ith day and do cool down on i+1 day> + prices[i], <cool-down on i+1 day> )
+                // dp[i][buy] = Math.max ( Math.max(dp[i+1][0] - prices[i], dp[i+1][1]), Math.max( dp[i+2][1] + prices[i], dp[i+1][0] ) )
+                // since cool-down is common, we can extract it
+                // dp[i][buy] = Max ( Max(dp[i+1][0] - prices[i] , dp[i+2][1] + prices[i]), dp[i+1][0] )
+
+                for (int i = n - 1; i >= 0; i--) {
+                    dp[i][1] = Math.max(dp[i + 1][0] - prices[i], dp[i + 1][1]);
+                    dp[i][0] = Math.max(dp[i + 2][1] + prices[i], dp[i + 1][0]);
+                }
+                return dp[0][1];
+
+            }
+
+
+        }
+
+        static class Solution_BottomUpTabulation_Optimized_SpaceOptimized {
+
+            public int maxProfit(int[] prices) {
+                int n = prices.length;
+
+
+                //dp[i][1] defines the profit gained by buying on day i
+                //dp[i][0] defines the profit gained by selling on day i
+                // dp[i][buy] = Math.max (
+                //                      Max ( <buy on ith day, sell on i+1 day> - prices[i] , cool-down on i+1 day> ),
+                //                      Max ( <sell on ith day and do cool down on i+1 day> + prices[i], <cool-down on i+1 day> )
+                // dp[i][buy] = Math.max ( Math.max(dp[i+1][0] - prices[i], dp[i+1][1]), Math.max( dp[i+2][1] + prices[i], dp[i+1][0] ) )
+                // since cool-down is common, we can extract it
+                // dp[i][buy] = Max ( Max(dp[i+1][0] - prices[i] , dp[i+2][1] + prices[i]), dp[i+1][0] )
+
+                // we are using only i+1 and i+2 and the current i, hence only 3 layers are being used.
+                // every day post-transaction, i+2 will be gone, 1+1 become i+2 day, and current i will be i+1 day
+                int[][] dp = new int[3][2]; // n days, with two possibilities buy or sell
+
+                for (int i = n - 1; i >= 0; i--) {
+                    dp[0][1] = Math.max(dp[1][0] - prices[i], dp[1][1]);
+                    dp[0][0] = Math.max(dp[2][1] + prices[i], dp[1][0]);
+
+                    //1 -> 2,
+                    dp[2][1] = dp[1][1];
+                    dp[2][0] = dp[1][0];
+
+                    // 0 -> 1
+                    dp[1][0] = dp[0][0];
+                    dp[1][1] = dp[0][1];
+
+                }
+                return dp[0][1];
+
+            }
+
+
+        }
+
+    }
 }
