db9a29c89269289439cfb80fcecb4f622a219a8b
nits2010 <iit.gupta2010@gmail.com>
2024-11-22 02:21:33 +0530

    MaxSumOfRectangleNoLargerThanK_363

--- src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Arrays/DynamicProgramming/matrix/_gfg/_27/MaximumSumRectangleInA2DMatrix_KadensAlgorithm2D_27.java
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Arrays/DynamicProgramming/matrix/_gfg/_27/MaximumSumRectangleInA2DMatrix_KadensAlgorithm2D_27.java
@@ -56,17 +56,26 @@ public class MaximumSumRectangleInA2DMatrix_KadensAlgorithm2D_27 {
         boolean pass, finalPass = true;
 
         KadensAlgorithm2D kadensAlgorithm2D = new KadensAlgorithm2D();
-
         output = kadensAlgorithm2D.maximumSumRectangle(mat);
         pass = CommonMethods.compareResultOutCome(output, expected, true);
         finalPass &= pass;
         CommonMethods.printTestOutcome(new String[]{"Output", "Pass"}, false, output, pass ? "Pass" : "Fail");
+
+        KadensAlgorithm2D_Optimized_PrefixSum_WithSubarray kadensAlgorithm2DOptimizedWithSubarray = new KadensAlgorithm2D_Optimized_PrefixSum_WithSubarray();
+        output = kadensAlgorithm2DOptimizedWithSubarray.maximumSumRectangle(mat);
+        pass = CommonMethods.compareResultOutCome(output, expected, true);
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"Output-Optimized-Prefix sum", "Pass"}, false, output, pass ? "Pass" : "Fail");
 
 
         return finalPass;
 
     }
 
+    /**
+     * T: O(col^2*(row + row)) = O(col^2*row)
+     * S: O(row)
+     */
     static class KadensAlgorithm2D {
         public int[] maximumSumRectangle(int[][] matrix) {
 
@@ -90,6 +99,7 @@ public class MaximumSumRectangleInA2DMatrix_KadensAlgorithm2D_27 {
                         temp[i] += matrix[i][rightColumn];
                     }
 
+                    // O(row)
                     int[] current = kadens1D(temp);
 
                     if (maxSum < current[0]) {
@@ -145,6 +155,131 @@ public class MaximumSumRectangleInA2DMatrix_KadensAlgorithm2D_27 {
 
             return max;
         }
+
+    }
+
+
+    static class KadensAlgorithm2D_Optimized_PrefixSum_WithSubarray {
+        public int[] maximumSumRectangle(int[][] matrix) {
+
+            int row = matrix.length;
+            int col = matrix[0].length;
+            int maxSum = Integer.MIN_VALUE;
+            int top = -1, bottom = -1, left = -1, right = -1;
+
+            //get the prefix sum of the matrix
+            //O(row*col)
+            for (int i = 0; i < row; i++) {
+                for (int j = 1; j < col; j++) {
+                    matrix[i][j] += matrix[i][j - 1];
+                }
+            }
+
+            // O(col)
+            for (int leftColumn = 0; leftColumn < col; leftColumn++) {
+
+                // O(col)
+                for (int rightColumn = leftColumn; rightColumn < col; rightColumn++) {
+                    int[] current = kadens1D(matrix, rightColumn, leftColumn, row);
+                    if (maxSum < current[0]) {
+
+                        maxSum = current[0];
+                        top = current[1];
+                        bottom = current[2];
+                        left = leftColumn;
+                        right = rightColumn;
+                    }
+
+                }
+
+            }
+            return new int[]{maxSum, top, bottom, left, right};
+
+        }
+
+        private int[] kadens1D(int[][] matrix, int rightColumn, int leftColumn, int row) {
+            int[] max = new int[]{Integer.MIN_VALUE, -1, -1}; // maxSum, left/top, right/bottom
+
+            int currentSum = 0;
+
+            int start = 0, end = 0;
+            int maxNeg = Integer.MIN_VALUE;
+            int maxNegIndex = -1;
+            for (int i = 0; i < row; i++) {
+                int sum = matrix[i][rightColumn] - (leftColumn == 0 ? 0 : matrix[i][leftColumn - 1]); // calculate the prefix sum at this point
+
+                currentSum += sum;
+
+                if (currentSum > max[0]) {
+                    max[0] = currentSum;
+                    max[1] = start;
+                    max[2] = i;
+                }
+
+                if (currentSum < 0) {
+                    currentSum = 0;
+                    start = i + 1;
+                }
+
+                if (sum < 0 && maxNeg < sum) {
+                    maxNeg = sum;
+                    maxNegIndex = i;
+                }
+
+            }
+
+            if (max[2] == -1) {
+                max[0] = maxNeg;
+                max[1] = maxNegIndex;
+                max[2] = maxNegIndex;
+            }
+
+            return max;
+        }
+
+    }
+
+    static class KadensAlgorithm2D_Optimized_PrefixSum {
+        public int maximumSumRectangle(int[][] matrix) {
+
+            int row = matrix.length;
+            int col = matrix[0].length;
+            int maxSum = Integer.MIN_VALUE;
+
+            //get the prefix sum of the matrix
+            //O(row*col)
+            for (int i = 0; i < row; i++) {
+                for (int j = 1; j < col; j++) {
+                    matrix[i][j] += matrix[i][j - 1];
+                }
+            }
+
+            // O(col)
+            for (int leftColumn = 0; leftColumn < col; leftColumn++) {
+
+                // O(col)
+                for (int rightColumn = leftColumn; rightColumn < col; rightColumn++) {
+                    int maxSumSub = Integer.MIN_VALUE;
+                    int currentSum = 0;
+                    //O(row)
+                    for (int i = 0; i < row; i++) {
+                        int sum = matrix[i][rightColumn] - (leftColumn == 0 ? 0 : matrix[i][leftColumn - 1]); // calculate the prefix sum at this point
+
+                        currentSum = Math.max(currentSum + sum, sum);
+                        maxSumSub = Math.max(maxSumSub, currentSum);
+                    }
+
+                    if (maxSum < maxSumSub) {
+
+                        maxSum = maxSumSub;
+                    }
+                }
+
+            }
+            return maxSum;
+
+        }
+
 
     }
 }
