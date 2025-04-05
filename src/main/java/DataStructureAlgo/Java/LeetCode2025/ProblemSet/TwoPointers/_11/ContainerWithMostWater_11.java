e9aef21982a446fd16ecd6a2d91008d4e2bb5ece
nits2010 <iit.gupta2010@gmail.com>
2025-04-05 13:04:12 +0530

    day3

---
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/TwoPointers/_11/ContainerWithMostWater_11.java
@@ -0,0 +1,164 @@
+package DataStructureAlgo.Java.LeetCode2025.ProblemSet.TwoPointers._11;
+
+import DataStructureAlgo.Java.helpers.*;
+
+import java.util.*;
+
+import DataStructureAlgo.Java.helpers.CommonMethods;
+
+
+/**
+ * Author: Nitin Gupta
+ * Date: 4/5/2025
+ * Question Title: 11. Container With Most Water
+ * Link:
+ * Description: You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ * <p>
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ * <p>
+ * Return the maximum amount of water a container can store.
+ * <p>
+ * Notice that you may not slant the container.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ * Example 2:
+ * <p>
+ * Input: height = [1,1]
+ * Output: 1
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * n == height.length
+ * 2 <= n <= 105
+ * 0 <= height[i] <= 104
+ * File reference
+ * -----------
+ * Duplicate {@link}
+ * Similar {@link}
+ * extension {@link }
+ * DP-BaseProblem {@link }
+ * <p><p>
+ * Tags
+ * -----
+ *
+ * @medium
+ * @Array
+ * @TwoPointers
+ * @Greedy <p><p>
+ * Company Tags
+ * -----
+ * @Amazon
+ * @Microsoft
+ * @Adobe
+ * @Facebook
+ * @Airbnb
+ * @Apple
+ * @Bloomberg
+ * @ByteDance
+ * @GoldmanSachs
+ * @Google
+ * @Lyft
+ * @Oracle
+ * @Uber
+ * @VMware
+ * @WalmartLabs
+ * @Yahoo <p>
+ * -----
+ * @Editorial <p><p>
+ * -----
+ * @OptimalSolution {@link }
+ */
+
+public class ContainerWithMostWater_11 {
+
+    public static void main(String[] args) {
+        List<Boolean> tests = new ArrayList<>();
+        tests.add(test(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}, 49));
+        tests.add(test(new int[]{1, 1}, 1));
+        tests.add(test(new int[]{4, 3, 2, 1, 4}, 16));
+
+        CommonMethods.printAllTestOutCome(tests);
+    }
+
+    private static boolean test(int[] height, int expected) {
+        //add print here
+        CommonMethods.printTestOutcome(new String[]{"Height", "Expected"}, true, height, expected);
+
+        int output = 0;
+        boolean pass, finalPass = true;
+
+        SolutionTwoPointer solutionTwoPointer = new SolutionTwoPointer();
+        SolutionTwoPointerOptimized solutionTwoPointerOptimized = new SolutionTwoPointerOptimized();
+        output = solutionTwoPointer.maxArea(height);
+        pass = CommonMethods.compareResultOutCome(output, expected, true);
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"Two Pointer", "Pass"}, false, output, pass ? "PASS" : "FAIL");
+
+        output = solutionTwoPointerOptimized.maxArea(height);
+        pass = CommonMethods.compareResultOutCome(output, expected, true);
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"Two Pointer Optimized", "Pass"}, false, output, pass ? "PASS" : "FAIL");
+
+        return finalPass;
+
+    }
+
+    static class SolutionTwoPointer {
+        public int maxArea(int[] height) {
+            int maxWater = 0;
+            int n = height.length;
+            int i = 0, j = n - 1;
+
+            while (i < j) {
+                int h, w;
+                if (height[i] <= height[j]) {
+                    h = height[i];
+                    w = j - i;
+
+                    i++;
+                } else {
+                    h = height[j];
+                    w = j - i;
+
+                    j--;
+                }
+
+                maxWater = Math.max(maxWater, h * w);
+            }
+            return maxWater;
+
+        }
+    }
+
+    static class SolutionTwoPointerOptimized {
+        public int maxArea(int[] height) {
+            int maxWater = 0;
+            int n = height.length;
+            int i = 0, j = n - 1;
+
+            while (i < j) {
+                int h = Math.min(height[i], height[j]);
+                int w = j - i;
+                maxWater = Math.max(maxWater, h * w);
+
+                //skip all smaller bars as they will always make water less
+                while (i < j && height[i] <= h)
+                    i++;
+
+                while (i < j && height[j] <= h)
+                    j--;
+
+            }
+            return maxWater;
+
+        }
+    }
+}
---
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/TwoPointers/_633/SumOfSquareNumbers_633.java
@@ -0,0 +1,122 @@
+package DataStructureAlgo.Java.LeetCode2025.ProblemSet.TwoPointers._633;
+
+import java.util.*;
+
+import DataStructureAlgo.Java.helpers.CommonMethods;
+
+
+/**
+ * Author: Nitin Gupta
+ * Date: 4/4/2025
+ * Question Title:633. Sum of Square Numbers
+ * Link: https://leetcode.com/problems/sum-of-square-numbers/description/
+ * Description: Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: c = 5
+ * Output: true
+ * Explanation: 1 * 1 + 2 * 2 = 5
+ * Example 2:
+ * <p>
+ * Input: c = 3
+ * Output: false
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= c <= 231 - 1
+ * File reference
+ * -----------
+ * Duplicate {@link}
+ * Similar {@link}
+ * extension {@link }
+ * DP-BaseProblem {@link }
+ * <p><p>
+ * Tags
+ * -----
+ *
+ * @medium
+ * @Math
+ * @TwoPointers
+ * @BinarySearch <p><p>
+ * Company Tags
+ * -----
+ * @Amazon
+ * @Bloomberg
+ * @LinkedIn <p>
+ * -----
+ * @Editorial https://leetcode.com/problems/sum-of-square-numbers/editorial <p><p>
+ * -----
+ * @OptimalSolution {@link }
+ */
+
+public class SumOfSquareNumbers_633 {
+
+    public static void main(String[] args) {
+        List<Boolean> tests = new ArrayList<>();
+        tests.add(test(5, true));
+        tests.add(test(3, false));
+        tests.add(test(4, true));
+        tests.add(test(0, true));
+        tests.add(test(2147483600, true));
+        CommonMethods.printAllTestOutCome(tests);
+    }
+
+    private static boolean test(int c, boolean expected) {
+        CommonMethods.printTestOutcome(new String[]{"c", "Expected"}, true, c, expected);
+
+        boolean output;
+        boolean pass, finalPass = true;
+
+        Solution solution = new Solution();
+        output = solution.judgeSquareSum(c);
+        pass = CommonMethods.compareResultOutCome(output, expected, true);
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"Two Pointer", "Pass"}, false, output, pass ? "PASS" : "FAIL");
+
+        SolutionPerfectSquare solutionPerfectSquare = new SolutionPerfectSquare();
+        output = solutionPerfectSquare.judgeSquareSum(c);
+        pass = CommonMethods.compareResultOutCome(output, expected, true);
+        finalPass &= pass;
+        CommonMethods.printTestOutcome(new String[]{"Perfect Square", "Pass"}, false, output, pass ? "PASS" : "FAIL");
+
+        CommonMethods.printTestOutcome(new String[]{"Output", "Pass"}, false, output, pass ? "PASS" : "FAIL");
+
+        return finalPass;
+
+    }
+
+    static class Solution {
+        public boolean judgeSquareSum(int c) {
+
+            long i = 0, j = (long) (Math.floor(Math.sqrt(c)));
+            while (i <= j) {
+                long square = i * i + j * j;
+                if (square == c)
+                    return true;
+                if (square > c)
+                    j--;
+                else
+                    i++;
+            }
+            return false;
+        }
+    }
+
+
+    static class SolutionPerfectSquare {
+        public boolean judgeSquareSum(int c) {
+            int n = (int) Math.sqrt(c);
+            for (int a = 0; a <= n; a++) {
+                double b = Math.sqrt(c - a * a);
+                if (b == (int) b) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
