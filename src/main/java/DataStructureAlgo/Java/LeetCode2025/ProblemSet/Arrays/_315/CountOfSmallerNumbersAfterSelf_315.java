3c4bbc9f537996842636451d0038fa599ae8249a
nits2010 <iit.gupta2010@gmail.com>
2025-06-14 23:25:43 +0530

    315. Count of Smaller Numbers After Self

--- src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Arrays/_315/CountOfSmallerNumbersAfterSelf_315.java
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Arrays/_315/CountOfSmallerNumbersAfterSelf_315.java
@@ -40,7 +40,7 @@ import java.util.*;
  * File reference
  * -----------
  * Duplicate {@link}
- * Similar {@link}
+ * Similar {@link DataStructureAlgo.Java.LeetCode2025.ProblemSet.Arrays._327.CountOfRangeSum_327}
  * extension {@link }
  * DP-BaseProblem {@link }
  * <p><p>
@@ -57,6 +57,13 @@ import java.util.*;
  * @OrderedSet <p><p>
  * Company Tags
  * -----
+ * @Amazon
+ * @Apple
+ * @Facebook
+ * @Google
+ * @Microsoft
+ * @Oracle
+ * @Uber
  * <p>
  * -----
  * @Editorial <p><p>
