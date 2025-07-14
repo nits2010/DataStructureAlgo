f7b3783d1d9b4ac523d360d2dbb99bd69625f7b7
nits2010 <iit.gupta2010@gmail.com>
2025-07-14 22:33:56 +0530

    2270. Number of Ways to Split Array

--- src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Arrays/_2270/NumberOfWaysToSplitArray_2270.py
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Arrays/_2270/NumberOfWaysToSplitArray_2270.py
@@ -5,7 +5,7 @@ Question Title: 2270. Number of Ways to Split Array
 Link: https://leetcode.com/problems/number-of-ways-to-split-array/description/
 Description: You are given a 0-indexed integer array nums of length n.
 
-nums contains a valid split at index i if the following are true:
+Nums contains a valid split at index i if the following are true:
 
 The sum of the first i + 1 elements is greater than or equal to the sum of the last n - i - 1 elements.
 There is at least one element to the right of i. That is, 0 <= i < n - 1.
