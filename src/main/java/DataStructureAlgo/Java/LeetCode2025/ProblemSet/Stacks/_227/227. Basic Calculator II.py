d84eb1e0b96b9cfa075c892bc998cf957b82912e
nits2010 <iit.gupta2010@gmail.com>
2025-07-18 23:05:56 +0530

    1079. Letter Tile Possibilities @medium

--- src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Stacks/_227/227. Basic Calculator II.py
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Stacks/_227/227. Basic Calculator II.py
@@ -32,7 +32,7 @@ Constraints:
 1 <= s.length <= 3 * 105
 s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
 s represents a valid expression.
-All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+All the integers in the expression are non-negative integers in the range [0, 2^31 - 1].
 The answer is guaranteed to fit in a 32-bit integer.
 File reference
 -----------
---
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/backtrackings/_1079/1079. Letter Tile Possibilities.py
@@ -0,0 +1,112 @@
+"""
+Author: Nitin Gupta
+Date: 7/18/2025
+Question Title: 1079. Letter Tile Possibilities
+Link: https://leetcode.com/problems/letter-tile-possibilities/description
+Description: You have n  tiles, where each tile has one letter tiles[i] printed on it.
+
+Return the number of possible non-empty sequences of letters you can make using the letters printed on those tiles.
+
+
+
+Example 1:
+
+Input: tiles = "AAB"
+Output: 8
+Explanation: The possible sequences are "A", "B", "AA", "AB", "BA", "AAB", "ABA", "BAA".
+Example 2:
+
+Input: tiles = "AAABBC"
+Output: 188
+Example 3:
+
+Input: tiles = "V"
+Output: 1
+
+
+Constraints:
+
+1 <= tiles.length <= 7
+tiles consists of uppercase English letters.
+File reference
+-----------
+Duplicate {@link}
+Similar {@link}
+extension {@link }
+DP-BaseProblem {@link }
+<p><p>
+Tags
+-----
+@medium
+@Backtracking
+@HashTable
+@String
+@Counting
+
+<p><p>
+Company Tags
+-----
+@Microsoft
+@Google
+<p>
+-----
+
+@Editorial <p><p>
+-----
+@OptimalSolution {@link }
+"""
+
+from collections import deque
+from typing import List, Optional, Dict, Any
+
+from helpers.common_methods import CommonMethods
+
+
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+
+        freq = [0] * 26
+
+        for ch in tiles:
+            freq[ord(ch) - ord('A')] += 1
+
+        def dfs():
+            sum = 0
+
+            for i in range(0, 26):
+                if freq[i] != 0:
+                    # Track
+                    freq[i] -= 1
+
+                    sum += 1  # count current char
+                    sum += dfs()
+
+                    # backtrack
+                    freq[i] += 1
+
+            return sum
+
+        return dfs()
+
+
+def test(input_data, expected):
+    """
+    Test function to verify the solution
+    """
+    CommonMethods.print_test(["Input", "Expected"], True, input_data, expected)
+    pass_test, final_pass = True, True
+    output = Solution().numTilePossibilities(input_data)
+    pass_test = CommonMethods.compare_result(output, expected, True)
+    CommonMethods.print_test(["Output", "Pass"], False, output, "PASS" if pass_test else "FAIL")
+    final_pass &= pass_test
+
+    return final_pass
+
+
+if __name__ == "__main__":
+    tests: List = [test("AAB", 8),
+                   test("AAABBC", 188),
+                   test("V", 1),
+                   test("XZ", 4), ]
+
+    CommonMethods.print_all_test_results(tests)
