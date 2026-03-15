3e4838a036877490e0cc8fc9ffc3515b63c3e73f
nits2010 <iit.gupta2010@gmail.com>
2026-03-16 00:25:09 +0530

    paint fence

--- src/main/java/DataStructureAlgo/Java/LeetCode/PaintFence.java
+++ src/main/java/DataStructureAlgo/Java/LeetCode/PaintFence.java
@@ -3,15 +3,15 @@ package DataStructureAlgo.Java.LeetCode;
 /**
  * Author: Nitin Gupta
  * Date: 24/09/19
- * Question Title: Paint Fence
+ * Question Title: 276. Paint Fence
  * Link: https://leetcode.com/problems/paint-fence
- * Description:
- * Description: https://leetcode.com/problems/paint-fence
  * http://leetcode.liangjiateng.cn/leetcode/paint-fence/description
- * 276.Paint Fence [EASY]
- * There is a fence with n posts, each post can be painted with one of the k colors.
+ * Description:
+ * There is a fence with n posts, each post can be painted with one of the k
+ * colors.
  * <p>
- * You have to paint all the posts such that no more than two adjacent fence posts have the same color.
+ * You have to paint all the posts such that no more than two adjacent fence
+ * posts have the same color.
  * <p>
  * Return the total number of ways you can paint the fence.
  * <p>
@@ -24,34 +24,42 @@ package DataStructureAlgo.Java.LeetCode;
  * Output: 6
  * Explanation: Take c1 as color 1, c2 as color 2. All possible ways are:
  * <p>
- * *           post1  post2  post3
- * -----      -----  -----  -----
- * 1         c1     c1     c2
- * 2         c1     c2     c1
- * 3         c1     c2     c2
- * 4         c2     c1     c1
- * 5         c2     c1     c2
- * 6         c2     c2     c1
+ * * post1 post2 post3
+ * ----- ----- ----- -----
+ * 1 c1 c1 c2
+ * 2 c1 c2 c1
+ * 3 c1 c2 c2
+ * 4 c2 c1 c1
+ * 5 c2 c1 c2
+ * 6 c2 c2 c1
  * <p>
- * https://www.youtube.com/watch?v=deh7UpSRaEY
- * https://www.geeksforgeeks.org/painting-fence-algorithm/
+ *
  * File reference
  * -----------
  * Duplicate {@link}
- * Similar {@link}
+ * Similar {@link PaintHouse_256.py}
  * extension {@link }
  * DP-BaseProblem {@link }
- * <p><p>
+ * <p>
+ * <p>
  * Tags
  * -----
+ * 
+ * @medium
+ * @DynamicProgramming
  *
- <p><p>
- * Company Tags
- * -----
- * <p>
- * -----
- * @Editorial <p><p>
- * -----
+ *                     <p>
+ *                     <p>
+ *                     Company Tags
+ *                     -----
+ * @Google
+ *         <p>
+ *         -----
+ * 
+ * @Editorial
+ *            https://www.youtube.com/watch?v=deh7UpSRaEY
+ *            https://www.geeksforgeeks.org/painting-fence-algorithm/
+ *            -----
  * @OptimalSolution {@link }
  */
 
@@ -78,32 +86,38 @@ public class PaintFence {
      * Lets take we have only two fence. [1,2] with k colors{K1,K2,...Kk}
      * Now there are two cases;
      * Case 1: Both fence has same color.
-     * *        If we paint the first fence with color = K1
-     * *        then we can paint the second fence with only same color = K1 i.e. we left with only one possibility in this case.
+     * * If we paint the first fence with color = K1
+     * * then we can paint the second fence with only same color = K1 i.e. we left with only one possibility in this case.
      * * Hence Same = k {k choices for first fence} * 1 {one choice for second to be same as first}
      * <p>
      * Case 2: Both fence has different color.
-     * *         If we paint the first fence with color = K1
-     * *         then we can paint the second fence with different color = {K2,...Kk} i.e. we left with only k-1  possibility in this case.
-     * * Hence different = k {k choices for first fence} * k-1 {k-1 choice for second to be different than  first}
+     * * If we paint the first fence with color = K1
+     * * then we can paint the second fence with different color = {K2,...Kk} i.e. we left with k-1 possibility in this case.
+     * * Hence different = k {k choices for first fence} * k-1 {k-1 choice for second to be different than first}
      * <p>
-     * Hence for 2 fence the answer is Same + different  = k*1 + k*(k-1) = k + k*(k-1)
+     * Hence for 2 fence the answer is Same + different = k*1 + k*(k-1) = k + k*(k-1)
      * <p>
      * <p>
      * Lets take we have only three fence. [1,2,3] with k colors{K1,K2,...Kk}
      * Case 1: Both last two fence has same color.
-     * * Since we can't have same color for three different fence in a row i.e. we can't choose 'same' from two fence. But we can choose 'different' from two fence, because 'different' has different color of
-     * last two fence. Since we need to paint last fence (3rd one) with same color as second last fence, then we left with only 1 choice.
+     * * Since we can't have same color for three different fence in a row i.e. we can't choose 'same' from two fence. But we can choose 'different' from two
+     * fence, because 'different' has different color of
+     * last two fence. Since we need to paint last fence (3rd one) with same color
+     * as second last fence, then we left with only 1 choice.
      * <p>
-     * Hence Same = different {choices when we paint last two fence with different color} * 1{for this fence, since we need same color as last fence}
+     * Hence Same = different {choices when we paint last two fence with different
+     * color} * 1{for this fence, since we need same color as last fence}
      * <p>
      * Case 1: Both last two fence has different color.
-     * * Since last two fence has different color i.e. we can put k-1 choices {since we need to make last fence has different color than second last fence}.
-     * * Which means either last two fence has same color and this one has different color Or last two has different color and this one also have different color
+     * * Since last two fence has different color i.e. we can put k-1 choices {since
+     * we need to make last fence has different color than second last fence}.
+     * * Which means either last two fence has same color and this one has different
+     * color Or last two has different color and this one also have different color
      * <p>
      * Hence different = (same + different) * (k-1)
      * <p>
-     * Hence for 3 fence the answer is Same + different  = (k*(k-1))*1  + (k*1 + k*(k-1)) * (k-1) =   2*k*(k-1)  + k*(k-1)^2
+     * Hence for 3 fence the answer is Same + different = (k*(k-1))*1 + (k*1 +
+     * k*(k-1)) * (k-1) = 2*k*(k-1) + k*(k-1)^2
      *
      * @param n
      * @param k
@@ -115,18 +129,20 @@ public class PaintFence {
             return 0;
 
         if (n == 1)
-            return k; //we have k choices to paint 1 fence
+            return k; // we have k choices to paint 1 fence
 
-        int same = k; // Same = k {k choices for first fence} * 1 {one choice for second to be same as first}
-        int different = k * (k - 1); // different = k {k choices for first fence} * k-1 {k-1 choice for second to be different than  first}
+        int same = k; // Same = k {k choices for first fence} * 1 {one choice for second to be same as
+                      // first}
+        int different = k * (k - 1); // different = k {k choices for first fence} * k-1 {k-1 choice for second to be
+                                     // different than first}
 
         for (int i = 3; i <= n; i++) {
             int prevDiff = different;
 
-            different = (same + different) * (k - 1); //different = (same + different) * (k-1)
-            same = prevDiff; //Same = different {choices when we paint last two fence with different color} * 1{for this fence, since we need same color as last fence}
+            different = (same + different) * (k - 1); // different = (same + different) * (k-1)
+            same = prevDiff; // Same = different {choices when we paint last two fence with different color}
+                             // * 1{for this fence, since we need same color as last fence}
         }
-
 
         return same + different;
     }
