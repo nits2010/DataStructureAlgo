dac2e6da0c674929d80879a7497ae42eed31cb66
nits2010 <iit.gupta2010@gmail.com>
2026-03-15 14:22:24 +0530

    272. Closest Binary Search Tree Value II

--- src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Trees/_272/ClosestBinarySearchTreeValueII_272.py
+++ src/main/java/DataStructureAlgo/Java/LeetCode2025/ProblemSet/Trees/_272/ClosestBinarySearchTreeValueII_272.py
@@ -40,6 +40,7 @@ Tags
 @TwoPointers
 @BinaryTree
 @Heap(PriorityQueue)
+@PremimumQuestion
 
 <p><p>
 Company Tags
@@ -65,6 +66,8 @@ from helpers.TreeBuilder import TreeBuilder
 from helpers.common_methods import CommonMethods
 
 # Definition for a binary tree node.
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -73,53 +76,54 @@ class TreeNode:
 
 # @ Time : O(k + log(n))
 # Space: O(k)
+
+
 class Solution_TwoStack_BalancedBST:
     """
     We need smallest diff k values from target. 
     Since tree is BST, the inorder traversal will generate a sorted list of values. 
     Additionally, this tree is balanced BST, which means for any value "point" in sorted list, has left and right part of it. 
-    
+
     If we find the "point" with respect to target, then it become a two pointer approach where we move either left or right based on which side element we choose as closest to target. 
-    
+
     In tree the left and right side of a point would be the predecessors and successors respectively. 
-    
+
     Hence we can get all the predecessors and successors of the point=target then, its matter of doing two pointer move (left and right). 
-    
+
     Algorithm:
     1. Get predecessors and successors wrt target. Any root.val <= target would be predecessors otherwise successors. We don't need all predecessors and successors in start
         we just need those nodes which leads predecessors and sucessors ( this way we won't iterate whole tree leading complexity = O(logn)).
         Once we find predecessors we move right side to get successors and similar to successors. 
         Store these nodes in stack, so that we can get predecessors and successors later
-        
+
     2. Now we have two stacks, pred_stack and succes_stack. Now the top of each stack is nothing but left and right of target. 
     3. We start comparing both the value and choose the smallest one and accordingly move either left (get next predecessors) or right (get next successors).
         We will repeate this K times only. 
     4. Any moment there is no predecessors or successors, the remaining stack will give rest of the values. 
-    
-    
+
+
     The Strategy: 
         Two Iterators (Stacks): We use two stacks to act as "iterators"—one for finding the next smallest values (predecessor) and one for finding the next largest values (successor).
         Initialize: Populate both stacks while searching for the target in O(\log N) time.
         Radiate: Compare the top of both stacks. Pick the one closer to the target, add it to your result, and move that specific iterator to its next value.  Repeat $k$ times.
-    
+
     Time Complexity:
         1. Build predecessors and successors: Since we just get in one direction max, we will explore max Log(n) nodes. Hence O(logn). Even if the target sits at the either leftmost or rightmost. 
         2. Getting next predecessors or successors depends on how many times it get called, which is at most O(k). However per call, we still get and push nodes in stack. 
             But we will touch each edge at max 2 times ( 1 time during build next time during next ). 
-        
-        Time: O(log(n) + K)
-    
-    Space: O(2k)
-    
-    """
-    
 
-    def build_predecessors_successors(self, root: TreeNode, target:float) :
+        Time: O(log(n) + K)
+
+    Space: O(2k)
+
+    """
+
+    def build_predecessors_successors(self, root: TreeNode, target: float):
         predecessors_stack = []  # nodes that are <= target
         successors_stack = []  # nodes that are > target
-        
+
         while root:
-            
+
             if root.val <= target:
                 predecessors_stack.append(root)
                 root = root.right
@@ -135,39 +139,40 @@ class Solution_TwoStack_BalancedBST:
 
         pred = stack.pop()
         curr = pred.left
-        
+
         while curr:
             stack.append(curr)
             curr = curr.right
-        
+
         return pred
-    
-    def get_successors(self, stack: list[TreeNode]) -> TreeNode : 
+
+    def get_successors(self, stack: list[TreeNode]) -> TreeNode:
         if not stack:
             return None
 
         succ = stack.pop()
         curr = succ.right
-        
+
         while curr:
             stack.append(curr)
             curr = curr.left
-        
+
         return succ
-    
+
     def closestKValues(self, root: TreeNode, target: float, k: int) -> List[int]:
         if not root:
             return []
 
-        predecessors_stack, successors_stack = self.build_predecessors_successors(root, target)
-        result = [] # result of k closest elements
-        
+        predecessors_stack, successors_stack = self.build_predecessors_successors(
+            root, target)
+        result = []  # result of k closest elements
+
         # set left and right pointers from center and expand towards left and right respectively, like two pointer
         pred, succ = self.get_predecessors(
             predecessors_stack), self.get_successors(successors_stack)
-        
+
         for _ in range(k):
-            
+
             if pred and succ:
                 if abs(pred.val - target) <= abs(succ.val - target):
                     result.append(pred.val)
@@ -181,11 +186,10 @@ class Solution_TwoStack_BalancedBST:
             elif succ:
                 result.append(succ.val)
                 succ = self.get_successors(successors_stack)
-                
-                    
+
         return result
-                    
-                    
+
+
 # Time O(n) or O(H)
 # Space:
 #     O(n+k) if implicit stack considered or O(k) for queue
@@ -201,7 +205,7 @@ class Solution_Queue_EarlyFinish:
 
         Time: queue operation O(1) done for all nodes in BST (n) worst case (if the target is at the very end of the sorted sequence),
         If tree is balanced BST then O(H) because of early exit
-        
+
         Time O(n) or O(H)
         Space: 
             O(n+k) if implicit stack considered or O(k) for queue
@@ -209,7 +213,7 @@ class Solution_Queue_EarlyFinish:
     """
 
     def __init__(self):
-        self.queue = None  
+        self.queue = None
         self.finished = False
 
     # O(1)
@@ -349,13 +353,13 @@ def test(tree, target, k, expected):
     CommonMethods.print_test(["Heap", "Pass"], False,
                              output, "PASS" if pass_test else "FAIL")
     final_pass &= pass_test
-    
+
     output = Solution_Queue().closestKValues(tree, target, k)
     pass_test = CommonMethods.compare_result(output, expected, True)
     CommonMethods.print_test(["Queue", "Pass"], False,
                              output, "PASS" if pass_test else "FAIL")
     final_pass &= pass_test
-    
+
     output = Solution_Queue_EarlyFinish().closestKValues(tree, target, k)
     pass_test = CommonMethods.compare_result(output, expected, True)
     CommonMethods.print_test(["Queue_EarlyFinish", "Pass"], False,
